a9ea2aff137f8506b603e2060bf0fb1921d77275
Aviu00 <aviu00@protonmail.com>
2025-10-07 16:24:23 +0300

    Remove EE parity.

--- Content.Goobstation.Common/CCVar/CCVars.Goob.cs
+++ Content.Goobstation.Common/CCVar/CCVars.Goob.cs
@@ -579,4 +579,15 @@ public sealed partial class GoobCVars
         CVarDef.Create("hub.use_dynamic_hostname", false, CVar.SERVERONLY);
 
     #endregion
+
+    // TODO: Move trauma cvars to traumamod when it's real
+    #region Trauma
+
+    /// <summary>
+    ///     Are height/width sliders enabled
+    /// </summary>
+    public static readonly CVarDef<bool> HeightSliders =
+        CVarDef.Create("trauma.height_sliders_enabled", false, CVar.SERVER | CVar.REPLICATED);
+
+    #endregion
 }
