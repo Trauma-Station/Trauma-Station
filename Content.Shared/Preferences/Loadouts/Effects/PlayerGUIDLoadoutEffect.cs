fbce1b796f7207c9a2156d1644037cab05e1a83a
Scribbles0 <scribbles0@outlook.com>
2025-10-20 13:09:36 -0700

    player exclusive loadout items

---
+++ Content.Shared/Preferences/Loadouts/Effects/PlayerGUIDLoadoutEffect.cs
@@ -0,0 +1,35 @@
+using Content.Shared.Preferences.Loadouts;
+using Content.Shared.Preferences;
+using Content.Shared.Preferences.Loadouts.Effects;
+using Robust.Shared.Player;
+using Robust.Shared.Utility;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace Content.Shared.Preferences.Loadouts.Effects;
+
+/// <summary>
+/// Checks for a specific player GUID.
+/// </summary>
+public sealed partial class PlayerGUIDLoadoutEffect : LoadoutEffect
+{
+    [DataField]
+    public string Guid {  get; set; }
+
+    public override bool Validate(HumanoidCharacterProfile profile, RoleLoadout loadout, ICommonSession? session, IDependencyCollection collection, [NotNullWhen(false)] out FormattedMessage? reason)
+    {
+        if (session == null)
+        {
+            reason = FormattedMessage.Empty;
+            return true;
+        }
+
+        if (session.UserId == new Guid(Guid))
+        {
+            reason = null;
+            return true;
+        }
+        reason = FormattedMessage.FromUnformatted(Loc.GetString("loadout-group-player-restriction"));
+        return false;
+    }
+}
