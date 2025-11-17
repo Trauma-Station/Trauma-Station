6c56f9de743528e154d740397b2c01f54ba24c6b
deltanedas <@deltanedas:kde.org>
2025-11-17 08:09:39 +0000

    implement pig latin

---
+++ Content.Trauma.Shared/Speech/PigLatinAccentComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Trauma.Shared.Speech;
+
+/// <summary>
+/// Accent that makes you speak in pig latin.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class PigLatinAccentComponent : Component;
