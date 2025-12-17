5fd79cda864d91bb1c5cacd988ce8ffbb30e51b8
GeneRickUsername <52056666+GeneRickUsername@users.noreply.github.com>
2025-12-17 01:33:18 -0800

    Update HolyIgniteOnMeleeHitComponent.cs

--- Content.Trauma.Server/Chaplain/Components/HolyIgniteOnMeleeHitComponent.cs
+++ Content.Trauma.Server/Chaplain/Components/HolyIgniteOnMeleeHitComponent.cs
@@ -1,6 +1,8 @@
 /// <summary>
 /// Component that can be used to add (or remove) holy fire stacks when used as a melee weapon.
 /// </summary>
+namespace Content.Trauma.Server.Chaplain.Components;
+
 [RegisterComponent]
 public sealed partial class HolyIgniteOnMeleeHitComponent : Component
 {
