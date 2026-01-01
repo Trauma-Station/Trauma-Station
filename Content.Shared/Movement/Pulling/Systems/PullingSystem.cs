4a88732fca9aea1340ece4edbd117ec2feaf862e
deltanedas <@deltanedas:kde.org>
2026-01-01 21:31:52 +0000

    playpredicted my beloved

--- Content.Shared/Movement/Pulling/Systems/PullingSystem.cs
+++ Content.Shared/Movement/Pulling/Systems/PullingSystem.cs
@@ -335,7 +335,7 @@ public sealed class PullingSystem : EntitySystem
             component.GrabThrownSpeed,
             damage * component.GrabThrowDamageModifier); // Throwing the grabbed person
         _throwing.TryThrow(uid, -direction * throwerPhysics.InvMass); // Throws back the grabber
-        _audio.PlayPvs(new SoundPathSpecifier("/Audio/Effects/thudswoosh.ogg"), uid);
+        _audio.PlayPredicted(new SoundPathSpecifier("/Audio/Effects/thudswoosh.ogg"), uid, uid);
         component.NextStageChange = _timing.CurTime.Add(TimeSpan.FromSeconds(3f)); // To avoid grab and throw spamming
     }
     // </Goob>
