947d3765f0085b331aaad07b82d124b00584f5cd
deltanedas <@deltanedas:kde.org>
2026-01-03 02:55:27 +0000

    intentionally not predict color flash

--- Content.Trauma.Shared/Projectiles/PredictedProjectileSystem.cs
+++ Content.Trauma.Shared/Projectiles/PredictedProjectileSystem.cs
@@ -81,7 +81,7 @@ public sealed class PredictedProjectileSystem : EntitySystem
 
         if (_damageable.TryChangeDamage((target, damageableComponent), ev.Damage, out var damage, component.IgnoreResistances, origin: component.Shooter, targetPart: targetPart) && Exists(component.Shooter))
         {
-            if (!deleted)
+            if (!deleted && _net.IsServer) // intentionally not predicting so you know if color flashes its 100% a hit
             {
                 _color.RaiseEffect(Color.Red, new List<EntityUid> { target }, Filter.Pvs(target, entityManager: EntityManager));
             }
