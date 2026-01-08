2b68c836a698be940edbc392f64f9410c6071206
deltanedas <@deltanedas:kde.org>
2026-01-08 10:50:37 +0000

    clean up nyano mail and other shitcode

--- Content.Trauma.Common/Mail/MailFragileEvent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Trauma.Common.Mail;
-
-/// <summary>
-/// Raised on mail to check if it is fragile.
-/// TODO MAIL: remove with nyano mail removal
-/// </summary>
-[ByRefEvent]
-public record struct MailFragileEvent(bool Fragile = false);
