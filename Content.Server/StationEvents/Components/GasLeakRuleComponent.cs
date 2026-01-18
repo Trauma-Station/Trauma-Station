0f2e2fb31fae60a87782d34b0aed427b06f5e732
catboycorporate <112313559+catboycorporate@users.noreply.github.com>
2026-01-18 00:27:04 +0000

    invisible instakill slop event

--- Content.Server/StationEvents/Components/GasLeakRuleComponent.cs
+++ Content.Server/StationEvents/Components/GasLeakRuleComponent.cs
@@ -26,7 +26,7 @@ public sealed partial class GasLeakRuleComponent : Component
         Gas.Tritium,
         Gas.Frezon,
         Gas.WaterVapor, // the fog
-        Gas.BZ, // Assmos - /tg/ gases
+        // Gas.BZ, // Trauma
         Gas.Healium, // Assmos - /tg/ gases
         Gas.Nitrium, // Assmos - /tg/ gases
         Gas.Pluoxium, // Assmos - /tg/ gases
