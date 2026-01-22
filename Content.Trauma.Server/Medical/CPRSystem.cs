d35aaa44385f82f98a4864f47552fdb39931aed1
deltanedas <@deltanedas:kde.org>
2026-01-22 16:34:17 +0000

    exhale as well

--- Content.Trauma.Server/Medical/CPRSystem.cs
+++ Content.Trauma.Server/Medical/CPRSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+using Content.Server.Body.Components;
 using Content.Server.Body.Systems;
 using Content.Trauma.Shared.Medical;
 
@@ -8,8 +9,21 @@ public sealed class CPRSystem : SharedCPRSystem
 {
     [Dependency] private readonly RespiratorSystem _respirator = default!;
 
+    private EntityQuery<RespiratorComponent> _respiratorQuery;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _respiratorQuery = GetEntityQuery<RespiratorComponent>();
+    }
+
     protected override void TryInhale(EntityUid uid)
     {
-        _respirator.Inhale(uid);
+        if (!_respiratorQuery.TryComp(uid, out var comp))
+            return;
+
+        _respirator.Inhale((uid, comp));
+        _respirator.Exhale((uid, comp)); // flush leftover gas to avoid gigadeath
     }
 }
