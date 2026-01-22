1d213716a0c79d6c00768c7689b0c8d2abedb9ca
deltanedas <@deltanedas:kde.org>
2026-01-22 13:01:09 +0000

    nuke unused rogue ascended shit and fix glyph examine

--- Content.Shared/_DV/Roles/RogueAscendedRoleComponent.cs
+++
@@ -1,9 +0,0 @@
-using Content.Shared.Roles.Components;
-
-namespace Content.Shared._DV.Roles;
-
-/// <summary>
-///     Added to mind role entities to tag that they are using the rogue ascended systems.
-/// </summary>
-[RegisterComponent]
-public sealed partial class RogueAscendedRoleComponent : BaseMindRoleComponent;
