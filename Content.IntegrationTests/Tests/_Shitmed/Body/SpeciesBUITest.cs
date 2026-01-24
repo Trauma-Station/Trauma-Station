bb6b3b79dabf46cec99a62ed640446f2edd7aaf1
deltanedas <@deltanedas:kde.org>
2026-01-24 15:11:38 +0000

    fix species bui test

--- Content.IntegrationTests/Tests/_Shitmed/Body/SpeciesBUITest.cs
+++ Content.IntegrationTests/Tests/_Shitmed/Body/SpeciesBUITest.cs
@@ -15,12 +15,14 @@ namespace Content.IntegrationTests.Tests._Shitmed.Body;
 [TestFixture]
 public sealed class SpeciesBUiTest
 {
+    private const string BaseMobSpeciesTest = "BaseMobSpeciesTest";
+
     [TestPrototypes]
-    private const string Prototypes = @"
+    private const string Prototypes = $@"
 - type: entity
-  name: BaseMobSpeciesTest
-  id: BaseMobSpeciesTest
   parent: BaseMobSpecies
+  id: {BaseMobSpeciesTest}
+  name: {BaseMobSpeciesTest}
 ";
 
     private Dictionary<Enum, InterfaceData> GetInterfaces(UserInterfaceComponent comp) =>
@@ -45,7 +47,7 @@ public sealed class SpeciesBUiTest
         {
             var bUiSys = server.System<SharedUserInterfaceSystem>();
 
-            var baseEnt = proto.Index("BaseMobSpeciesTest");
+            var baseEnt = proto.Index(BaseMobSpeciesTest);
             Assert.That(baseEnt, Is.Not.Null);
             Assert.That(baseEnt.TryGetComponent<UserInterfaceComponent>(out var bUiBase, factoryComp), Is.True);
             Assert.That(bUiBase, Is.Not.Null);
@@ -56,7 +58,7 @@ public sealed class SpeciesBUiTest
                 var ent = proto.Index(species.Prototype);
                 Assert.That(ent.TryGetComponent<UserInterfaceComponent>(out var bUi, factoryComp), Is.True);
                 Assert.That(bUi, Is.Not.Null);
-                var states = GetInterfaces(bUiBase);
+                var states = GetInterfaces(bUi);
                 foreach (var key in baseKeys)
                 {
                     Assert.That(states.ContainsKey(key), Is.True, $"Species {species.ID} has not UserInterface of type enum.{key.GetType().Name}");
