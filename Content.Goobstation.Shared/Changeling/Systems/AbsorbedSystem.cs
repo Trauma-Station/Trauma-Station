5f15ee8298d650e20a903badc0c9b69dd1e3c99f
deltanedas <@deltanedas:kde.org>
2026-01-31 15:43:46 +0000

    ling butchering refactor

--- Content.Goobstation.Shared/Changeling/Systems/AbsorbedSystem.cs
+++ Content.Goobstation.Shared/Changeling/Systems/AbsorbedSystem.cs
@@ -9,7 +9,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Goobstation.Common.Changeling;
+using Content.Goobstation.Shared.Changeling.Components;
 using Content.Shared.Examine;
 using Content.Shared.Mobs;
 
