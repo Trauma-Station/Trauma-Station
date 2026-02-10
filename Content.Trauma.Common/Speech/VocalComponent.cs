b9cb54ca3d7bce1ccca3ed69ae1599bfbe248b1e
No Elka <no.elka.the.god@gmail.com>
2026-02-10 15:46:05 +0300

    Okay bruh

---
+++ Content.Trauma.Common/Speech/VocalComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared.Speech.Components;
+
+/// <summary>
+///     Raised when the component's Sounds field changes.
+/// </summary>
+[ByRefEvent]
+public record struct SoundsChangedEvent();
