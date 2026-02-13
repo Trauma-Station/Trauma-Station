madeline-celeste/Maidstation

ooplink cleanup (#5915)
* mraow

* leguia pissed on my events

* leguia pissed on my uplink briefing

Content.IntegrationTests/Tests/Goobstation/UplinkPreferenceTests.cs
@@ -5,11 +5,13 @@ using Content.IntegrationTests.Pair;
 using Content.Server.GameTicking;
 using Content.Server.Traitor.Uplink;
 using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Implants.Components;
 using Content.Shared.PDA;
+using Content.Shared.Store;
 using Content.Shared.Store.Components;
-using Content.Shared.Tag;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
 
 namespace Content.IntegrationTests.Tests.Goobstation;
 
@@ -19,6 +21,8 @@ public sealed class UplinkPreferenceTests
 {
     private TestPair _pair = default!;
     private EntityUid _player;
+
+    private static readonly ProtoId<UplinkPreferencePrototype> PenPreference = "UplinkPen";
 
     [SetUp]
     public async Task Setup()
@@ -73,11 +77,11 @@ public sealed class UplinkPreferenceTests
     {
         var server = _pair.Server;
         var entMan = server.EntMan;
-        var uplinkSys = server.System<UplinkSystem>();
+        var goobUplinkSys = server.System<GoobCommonUplinkSystem>();
 
         await server.WaitAssertion(() =>
         {
-            var pdaTarget = uplinkSys.FindPdaUplinkTarget(_player);
+            var pdaTarget = goobUplinkSys.FindUplinkTarget(_player, new[] { "Pda" });
             Assert.That(pdaTarget, Is.Not.Null, "Player should have a PDA");
             Assert.That(entMan.HasComponent<PdaComponent>(pdaTarget!.Value), Is.True);
         });
@@ -87,66 +91,16 @@ public sealed class UplinkPreferenceTests
     public async Task TestFindPenUplinkTarget()
     {
         var server = _pair.Server;
-        var goobUplinkSys = server.System<GoobCommonUplinkSystem>();
-        var tagSys = server.System<TagSystem>();
-
-        await SpawnPenInHand();
-
-        await server.WaitAssertion(() =>
-        {
-            var penTarget = goobUplinkSys.FindPenUplinkTarget(_player);
-            Assert.That(penTarget, Is.Not.Null, "Player should have a pen");
-            Assert.That(tagSys.HasTag(penTarget!.Value, "Pen"), Is.True);
-        });
-    }
-
-    [Test]
-    public async Task TestAddUplinkPdaPreference()
-    {
-        var server = _pair.Server;
         var entMan = server.EntMan;
-        var uplinkSys = server.System<UplinkSystem>();
-
-        await server.WaitAssertion(() =>
-        {
-            var pdaTarget = uplinkSys.FindPdaUplinkTarget(_player);
-            Assert.That(pdaTarget, Is.Not.Null, "Player should have a PDA");
-
-            var result = uplinkSys.AddUplink(_player, 20, UplinkPreference.Pda);
-            Assert.That(result, Is.True);
-
-            Assert.That(entMan.HasComponent<StoreComponent>(pdaTarget!.Value), Is.True);
-            var store = entMan.GetComponent<StoreComponent>(pdaTarget.Value);
-            Assert.That(store.Balance.ContainsKey("Telecrystal"), Is.True);
-            Assert.That((int) store.Balance["Telecrystal"], Is.EqualTo(20));
-        });
-    }
-
-    [Test]
-    public async Task TestAddUplinkPenPreference()
-    {
-        var server = _pair.Server;
-        var entMan = server.EntMan;
-        var uplinkSys = server.System<UplinkSystem>();
         var goobUplinkSys = server.System<GoobCommonUplinkSystem>();
 
         await SpawnPenInHand();
 
         await server.WaitAssertion(() =>
         {
-            var penTarget = goobUplinkSys.FindPenUplinkTarget(_player);
+            var penTarget = goobUplinkSys.FindUplinkTarget(_player, new[] { "Pen" });
             Assert.That(penTarget, Is.Not.Null, "Player should have a pen");
-
-            var result = uplinkSys.AddUplink(_player, 20, UplinkPreference.Pen);
-            Assert.That(result, Is.True);
-
-            Assert.That(entMan.HasComponent<PenSpinComponent>(penTarget!.Value), Is.True);
-            Assert.That(entMan.HasComponent<PenSpinUplinkComponent>(penTarget.Value), Is.True);
-            Assert.That(entMan.HasComponent<StoreComponent>(penTarget.Value), Is.True);
-
-            var store = entMan.GetComponent<StoreComponent>(penTarget.Value);
-            Assert.That(store.Balance.ContainsKey("Telecrystal"), Is.True);
-            Assert.That((int) store.Balance["Telecrystal"], Is.EqualTo(20));
+            Assert.That(entMan.HasComponent<PenComponent>(penTarget!.Value), Is.True);
         });
     }
 
@@ -160,21 +114,18 @@ public sealed class UplinkPreferenceTests
 
         await SpawnPenInHand();
 
-        await server.WaitPost(() => uplinkSys.AddUplink(_player, 20, UplinkPreference.Pen));
+        await server.WaitPost(() => uplinkSys.TryAddUplink(_player, 20, PenPreference, out _, out _));
         await _pair.RunTicksSync(5);
 
         await server.WaitAssertion(() =>
         {
-            var penTarget = goobUplinkSys.FindPenUplinkTarget(_player);
+            var penTarget = goobUplinkSys.FindUplinkTarget(_player, new[] { "Pen" });
             Assert.That(penTarget, Is.Not.Null);
 
-            var spinComp = entMan.GetComponent<PenSpinComponent>(penTarget!.Value);
+            var spinComp = entMan.GetComponent<PenComponent>(penTarget!.Value);
             Assert.That(spinComp.CombinationLength, Is.EqualTo(4));
             Assert.That(spinComp.MinDegree, Is.EqualTo(0));
             Assert.That(spinComp.MaxDegree, Is.EqualTo(359));
-
-            var ev = new GeneratePenSpinCodeEvent();
-            entMan.EventBus.RaiseLocalEvent(penTarget.Value, ref ev);
 
             var uplinkComp = entMan.GetComponent<PenSpinUplinkComponent>(penTarget.Value);
             Assert.That(uplinkComp.Code, Is.Not.Null);
@@ -189,15 +140,94 @@ public sealed class UplinkPreferenceTests
     }
 
     [Test]
-    public async Task TestAddUplinkImplantPreference()
+    public async Task TestAllUplinkPreferences()
     {
         var server = _pair.Server;
+        var entMan = server.EntMan;
+        var protoMan = server.ProtoMan;
         var uplinkSys = server.System<UplinkSystem>();
+        var handsSys = server.System<SharedHandsSystem>();
+
+        foreach (var pref in protoMan.EnumeratePrototypes<UplinkPreferencePrototype>())
+        {
+            if (pref.SearchComponents != null)
+            {
+                await server.WaitPost(() =>
+                {
+                    if (Array.IndexOf(pref.SearchComponents, "Pen") >= 0)
+                    {
+                        var coords = entMan.GetComponent<TransformComponent>(_player).Coordinates;
+                        var pen = entMan.SpawnEntity("Pen", coords);
+                        handsSys.TryPickupAnyHand(_player, pen);
+                    }
+                });
+                await _pair.RunTicksSync(5);
+            }
+
+            await server.WaitAssertion(() =>
+            {
+                var success = uplinkSys.TryAddUplink(_player, 20, pref.ID, out var uplinkTarget, out var setupEvent);
+                Assert.That(success, Is.True, $"TryAddUplink failed for preference {pref.ID}");
+
+                if (pref.SearchComponents != null)
+                {
+                    Assert.That(uplinkTarget, Is.Not.Null, $"Should find uplink target for {pref.ID}");
+                    Assert.That(setupEvent, Is.Not.Null, $"SetupUplinkEvent should be raised for {pref.ID}");
+                    Assert.That(setupEvent!.Value.Handled, Is.True, $"SetupUplinkEvent should be handled for {pref.ID}");
+
+                    var store = entMan.GetComponent<StoreComponent>(uplinkTarget!.Value);
+                    Assert.That(store.Balance.ContainsKey("Telecrystal"), Is.True, $"Store should have TC for {pref.ID}");
+                    Assert.That((int) store.Balance["Telecrystal"], Is.EqualTo(20), $"Store should have 20 TC for {pref.ID}");
+                }
+                else // Fallback
+                {
+                    Assert.That(uplinkTarget, Is.Null, $"Implant preference {pref.ID} should have no target entity");
+                    Assert.That(setupEvent, Is.Null, $"Implant preference {pref.ID} should have no setup event");
+                }
+            });
+        }
+    }
+
+    [Test]
+    public async Task TestImplantUplinkBalance()
+    {
+        var server = _pair.Server;
+        var entMan = server.EntMan;
+        var protoMan = server.ProtoMan;
+        var uplinkSys = server.System<UplinkSystem>();
+
+        const int startingBalance = 100;
+        var implantPreference = new ProtoId<UplinkPreferencePrototype>("UplinkImplant");
 
         await server.WaitAssertion(() =>
         {
-            var result = uplinkSys.AddUplink(_player, 20, UplinkPreference.Implant);
-            Assert.That(result, Is.True);
+            var success = uplinkSys.TryAddUplink(_player, startingBalance, implantPreference, out var uplinkTarget, out _);
+            Assert.That(success, Is.True, "Implant uplink should succeed");
+            Assert.That(uplinkTarget, Is.Null, "Implant preference should not return an uplink target entity");
+
+            Assert.That(entMan.TryGetComponent<ImplantedComponent>(_player, out var implanted), Is.True,
+                "Player should have ImplantedComponent after implant uplink");
+
+            EntityUid? implantStore = null;
+            foreach (var implantEnt in implanted!.ImplantContainer.ContainedEntities)
+            {
+                if (entMan.HasComponent<StoreComponent>(implantEnt))
+                {
+                    implantStore = implantEnt;
+                    break;
+                }
+            }
+
+            Assert.That(implantStore, Is.Not.Null, "Should find a store component on the implant");
+
+            var store = entMan.GetComponent<StoreComponent>(implantStore!.Value);
+            Assert.That(store.Balance.ContainsKey("Telecrystal"), Is.True);
+
+            var catalog = protoMan.Index<ListingPrototype>("UplinkUplinkImplanter");
+            var implantCost = (int) catalog.Cost["Telecrystal"];
+            var expectedBalance = startingBalance - implantCost;
+            Assert.That((int) store.Balance["Telecrystal"], Is.EqualTo(expectedBalance),
+                $"Implant store should have {expectedBalance} TC (starting {startingBalance} minus {implantCost} implant cost)");
         });
     }
 }
@@ -205,6 +235,8 @@ public sealed class UplinkPreferenceTests
 [TestFixture]
 public sealed class UplinkFallbackTests
 {
+    private static readonly ProtoId<UplinkPreferencePrototype> PenPreference = "UplinkPen";
+
     [Test]
     public async Task TestAddUplinkFallbackToImplant()
     {
@@ -223,11 +255,11 @@ public sealed class UplinkFallbackTests
 
         await server.WaitAssertion(() =>
         {
-            var penTarget = goobUplinkSys.FindPenUplinkTarget(dummy);
+            var penTarget = goobUplinkSys.FindUplinkTarget(dummy, new[] { "Pen" });
             Assert.That(penTarget, Is.Null, "Dummy should not have a pen");
 
-            var result = uplinkSys.AddUplink(dummy, 20, UplinkPreference.Pen);
-            Assert.That(result, Is.True, "Should fall back to implant when pen unavailable");
+            var success = uplinkSys.TryAddUplink(dummy, 20, PenPreference, out _, out _);
+            Assert.That(success, Is.True, "Should fall back to implant when pen unavailable");
         });
 
         await pair.CleanReturnAsync();

Trauma-Station/Trauma-Station

Content.IntegrationTests/Tests/_Goobstation/UplinkPreferenceTests.cs
@@ -5,21 +5,24 @@ using Content.IntegrationTests.Pair;
 using Content.Server.GameTicking;
 using Content.Server.Traitor.Uplink;
 using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Implants.Components;
 using Content.Shared.PDA;
+using Content.Shared.Store;
 using Content.Shared.Store.Components;
-using Content.Shared.Tag;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
 
 namespace Content.IntegrationTests.Tests._Goobstation;
 
-/*
 [TestFixture]
 [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
 public sealed class UplinkPreferenceTests
 {
     private TestPair _pair = default!;
     private EntityUid _player;
+
+    private static readonly ProtoId<UplinkPreferencePrototype> PenPreference = "UplinkPen";
 
     [SetUp]
     public async Task Setup()
@@ -74,11 +77,11 @@ public sealed class UplinkPreferenceTests
     {
         var server = _pair.Server;
         var entMan = server.EntMan;
-        var uplinkSys = server.System<UplinkSystem>();
+        var goobUplinkSys = server.System<GoobCommonUplinkSystem>();
 
         await server.WaitAssertion(() =>
         {
-            var pdaTarget = uplinkSys.FindPdaUplinkTarget(_player);
+            var pdaTarget = goobUplinkSys.FindUplinkTarget(_player, new[] { "Pda" });
             Assert.That(pdaTarget, Is.Not.Null, "Player should have a PDA");
             Assert.That(entMan.HasComponent<PdaComponent>(pdaTarget!.Value), Is.True);
         });
@@ -88,66 +91,16 @@ public sealed class UplinkPreferenceTests
     public async Task TestFindPenUplinkTarget()
     {
         var server = _pair.Server;
-        var goobUplinkSys = server.System<GoobCommonUplinkSystem>();
-        var tagSys = server.System<TagSystem>();
-
-        await SpawnPenInHand();
-
-        await server.WaitAssertion(() =>
-        {
-            var penTarget = goobUplinkSys.FindPenUplinkTarget(_player);
-            Assert.That(penTarget, Is.Not.Null, "Player should have a pen");
-            Assert.That(tagSys.HasTag(penTarget!.Value, "Pen"), Is.True);
-        });
-    }
-
-    [Test]
-    public async Task TestAddUplinkPdaPreference()
-    {
-        var server = _pair.Server;
         var entMan = server.EntMan;
-        var uplinkSys = server.System<UplinkSystem>();
-
-        await server.WaitAssertion(() =>
-        {
-            var pdaTarget = uplinkSys.FindPdaUplinkTarget(_player);
-            Assert.That(pdaTarget, Is.Not.Null, "Player should have a PDA");
-
-            var result = uplinkSys.AddUplink(_player, 20, UplinkPreference.Pda);
-            Assert.That(result, Is.True);
-
-            Assert.That(entMan.HasComponent<StoreComponent>(pdaTarget!.Value), Is.True);
-            var store = entMan.GetComponent<StoreComponent>(pdaTarget.Value);
-            Assert.That(store.Balance.ContainsKey("Telecrystal"), Is.True);
-            Assert.That((int) store.Balance["Telecrystal"], Is.EqualTo(20));
-        });
-    }
-
-    [Test]
-    public async Task TestAddUplinkPenPreference()
-    {
-        var server = _pair.Server;
-        var entMan = server.EntMan;
-        var uplinkSys = server.System<UplinkSystem>();
         var goobUplinkSys = server.System<GoobCommonUplinkSystem>();
 
         await SpawnPenInHand();
 
         await server.WaitAssertion(() =>
         {
-            var penTarget = goobUplinkSys.FindPenUplinkTarget(_player);
+            var penTarget = goobUplinkSys.FindUplinkTarget(_player, new[] { "Pen" });
             Assert.That(penTarget, Is.Not.Null, "Player should have a pen");
-
-            var result = uplinkSys.AddUplink(_player, 20, UplinkPreference.Pen);
-            Assert.That(result, Is.True);
-
-            Assert.That(entMan.HasComponent<PenSpinComponent>(penTarget!.Value), Is.True);
-            Assert.That(entMan.HasComponent<PenSpinUplinkComponent>(penTarget.Value), Is.True);
-            Assert.That(entMan.HasComponent<StoreComponent>(penTarget.Value), Is.True);
-
-            var store = entMan.GetComponent<StoreComponent>(penTarget.Value);
-            Assert.That(store.Balance.ContainsKey("Telecrystal"), Is.True);
-            Assert.That((int) store.Balance["Telecrystal"], Is.EqualTo(20));
+            Assert.That(entMan.HasComponent<PenComponent>(penTarget!.Value), Is.True);
         });
     }
 
@@ -161,21 +114,18 @@ public sealed class UplinkPreferenceTests
 
         await SpawnPenInHand();
 
-        await server.WaitPost(() => uplinkSys.AddUplink(_player, 20, UplinkPreference.Pen));
+        await server.WaitPost(() => uplinkSys.AddUplink(_player, 100, PenPreference, out _, out _));
         await _pair.RunTicksSync(5);
 
         await server.WaitAssertion(() =>
         {
-            var penTarget = goobUplinkSys.FindPenUplinkTarget(_player);
+            var penTarget = goobUplinkSys.FindUplinkTarget(_player, new[] { "Pen" });
             Assert.That(penTarget, Is.Not.Null);
 
-            var spinComp = entMan.GetComponent<PenSpinComponent>(penTarget!.Value);
+            var spinComp = entMan.GetComponent<PenComponent>(penTarget!.Value);
             Assert.That(spinComp.CombinationLength, Is.EqualTo(4));
             Assert.That(spinComp.MinDegree, Is.EqualTo(0));
             Assert.That(spinComp.MaxDegree, Is.EqualTo(359));
-
-            var ev = new GeneratePenSpinCodeEvent();
-            entMan.EventBus.RaiseLocalEvent(penTarget.Value, ref ev);
 
             var uplinkComp = entMan.GetComponent<PenSpinUplinkComponent>(penTarget.Value);
             Assert.That(uplinkComp.Code, Is.Not.Null);
@@ -190,15 +140,94 @@ public sealed class UplinkPreferenceTests
     }
 
     [Test]
-    public async Task TestAddUplinkImplantPreference()
+    public async Task TestAllUplinkPreferences()
     {
         var server = _pair.Server;
+        var entMan = server.EntMan;
+        var protoMan = server.ProtoMan;
         var uplinkSys = server.System<UplinkSystem>();
+        var handsSys = server.System<SharedHandsSystem>();
+
+        foreach (var pref in protoMan.EnumeratePrototypes<UplinkPreferencePrototype>())
+        {
+            if (pref.SearchComponents != null)
+            {
+                await server.WaitPost(() =>
+                {
+                    if (Array.IndexOf(pref.SearchComponents, "Pen") >= 0)
+                    {
+                        var coords = entMan.GetComponent<TransformComponent>(_player).Coordinates;
+                        var pen = entMan.SpawnEntity("Pen", coords);
+                        handsSys.TryPickupAnyHand(_player, pen);
+                    }
+                });
+                await _pair.RunTicksSync(5);
+            }
+
+            await server.WaitAssertion(() =>
+            {
+                var success = uplinkSys.AddUplink(_player, 100, pref.ID, out var uplinkTarget, out var setupEvent);
+                Assert.That(success, Is.True, $"TryAddUplink failed for preference {pref.ID}");
+
+                if (pref.SearchComponents != null)
+                {
+                    Assert.That(uplinkTarget, Is.Not.Null, $"Should find uplink target for {pref.ID}");
+                    Assert.That(setupEvent, Is.Not.Null, $"SetupUplinkEvent should be raised for {pref.ID}");
+                    Assert.That(setupEvent!.Value.Handled, Is.True, $"SetupUplinkEvent should be handled for {pref.ID}");
+
+                    var store = entMan.GetComponent<StoreComponent>(uplinkTarget!.Value);
+                    Assert.That(store.Balance.ContainsKey("Telecrystal"), Is.True, $"Store should have TC for {pref.ID}");
+                    Assert.That((int) store.Balance["Telecrystal"], Is.EqualTo(20), $"Store should have 20 TC for {pref.ID}");
+                }
+                else // Fallback
+                {
+                    Assert.That(uplinkTarget, Is.Null, $"Implant preference {pref.ID} should have no target entity");
+                    Assert.That(setupEvent, Is.Null, $"Implant preference {pref.ID} should have no setup event");
+                }
+            });
+        }
+    }
+
+    [Test]
+    public async Task TestImplantUplinkBalance()
+    {
+        var server = _pair.Server;
+        var entMan = server.EntMan;
+        var protoMan = server.ProtoMan;
+        var uplinkSys = server.System<UplinkSystem>();
+
+        const int startingBalance = 100;
+        var implantPreference = new ProtoId<UplinkPreferencePrototype>("UplinkImplant");
 
         await server.WaitAssertion(() =>
         {
-            var result = uplinkSys.AddUplink(_player, 20, UplinkPreference.Implant);
-            Assert.That(result, Is.True);
+            var success = uplinkSys.AddUplink(_player, startingBalance, implantPreference, out var uplinkTarget, out _);
+            Assert.That(success, Is.True, "Implant uplink should succeed");
+            Assert.That(uplinkTarget, Is.Null, "Implant preference should not return an uplink target entity");
+
+            Assert.That(entMan.TryGetComponent<ImplantedComponent>(_player, out var implanted), Is.True,
+                "Player should have ImplantedComponent after implant uplink");
+
+            EntityUid? implantStore = null;
+            foreach (var implantEnt in implanted!.ImplantContainer.ContainedEntities)
+            {
+                if (entMan.HasComponent<StoreComponent>(implantEnt))
+                {
+                    implantStore = implantEnt;
+                    break;
+                }
+            }
+
+            Assert.That(implantStore, Is.Not.Null, "Should find a store component on the implant");
+
+            var store = entMan.GetComponent<StoreComponent>(implantStore!.Value);
+            Assert.That(store.Balance.ContainsKey("Telecrystal"), Is.True);
+
+            var catalog = protoMan.Index<ListingPrototype>("UplinkUplinkImplanter");
+            var implantCost = (int) catalog.Cost["Telecrystal"];
+            var expectedBalance = startingBalance - implantCost;
+            Assert.That((int) store.Balance["Telecrystal"], Is.EqualTo(expectedBalance),
+                $"Implant store should have {expectedBalance} TC (starting {startingBalance} minus {implantCost} implant cost)");
         });
     }
 }
@@ -206,6 +235,8 @@ public sealed class UplinkPreferenceTests
 [TestFixture]
 public sealed class UplinkFallbackTests
 {
+    private static readonly ProtoId<UplinkPreferencePrototype> PenPreference = "UplinkPen";
+
     [Test]
     public async Task TestAddUplinkFallbackToImplant()
     {
@@ -224,14 +255,13 @@ public sealed class UplinkFallbackTests
 
         await server.WaitAssertion(() =>
         {
-            var penTarget = goobUplinkSys.FindPenUplinkTarget(dummy);
+            var penTarget = goobUplinkSys.FindUplinkTarget(dummy, new[] { "Pen" });
             Assert.That(penTarget, Is.Null, "Dummy should not have a pen");
 
-            var result = uplinkSys.AddUplink(dummy, 20, UplinkPreference.Pen);
-            Assert.That(result, Is.True, "Should fall back to implant when pen unavailable");
+            var success = uplinkSys.AddUplink(dummy, 100, PenPreference, out _, out _);
+            Assert.That(success, Is.True, "Should fall back to implant when pen unavailable");
         });
 
         await pair.CleanReturnAsync();
     }
 }
-*/