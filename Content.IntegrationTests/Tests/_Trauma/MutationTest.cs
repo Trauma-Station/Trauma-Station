36435c0586330adb34f04b2aff5efbe6ee2bf725
deltanedas <@deltanedas:kde.org>
2026-02-16 19:54:19 +0000

    a

--- Content.IntegrationTests/Tests/_Trauma/MutationTest.cs
+++ Content.IntegrationTests/Tests/_Trauma/MutationTest.cs
@@ -34,7 +34,7 @@ public sealed class MutationTest
                 {
                     var mob = entMan.SpawnEntity(TestMob, map.GridCoords);
                     Assert.That(mutation.AddMutation(mob, id), $"Failed to add {id} to {entMan.ToPrettyString(mob)}");
-                    Assert.That(mutation.HasMutation(mob, id), $"Added {id} but it was not present in {entMan.ToPrettyStirng(mob)}");
+                    Assert.That(mutation.HasMutation(mob, id), $"Added {id} but it was not present in {entMan.ToPrettyString(mob)}");
                     mobs.Add(mob);
                 }
             });
