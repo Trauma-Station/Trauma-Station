e0777f3168af40f9f9327a64921bf9804e02e8c7
deltanedas <@deltanedas:kde.org>
2026-02-16 21:15:44 +0000

    to the man who sold the world

--- Content.Shared/Body/Systems/SharedBloodstreamSystem.Trauma.cs
+++ Content.Shared/Body/Systems/SharedBloodstreamSystem.Trauma.cs
@@ -22,6 +22,6 @@ public abstract partial class SharedBloodstreamSystem
     public void SetRefreshAmount(Entity<BloodstreamComponent> ent, FixedPoint2 amount)
     {
         ent.Comp.BloodRefreshAmount = amount;
-        DirtyField(ent, nameof(BloodstreamComponent.BloodRefreshAmount));
+        DirtyField(ent.AsNullable(), nameof(BloodstreamComponent.BloodRefreshAmount));
     }
 }
