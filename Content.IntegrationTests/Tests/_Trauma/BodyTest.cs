250255d47c97f6bbd69d4af4e1220a8c06793d9c
deltanedas <@deltanedas:kde.org>
2026-02-18 17:16:03 +0000

    !

--- Content.IntegrationTests/Tests/_Trauma/BodyTest.cs
+++ Content.IntegrationTests/Tests/_Trauma/BodyTest.cs
@@ -32,7 +32,7 @@ public sealed class BodyTest
             {
                 foreach (var proto in protoMan.EnumeratePrototypes<EntityPrototype>())
                 {
-                    if (proto.IsTestPrototype || !proto.Components.ContainsKey(bodyName))
+                    if (pair.IsTestPrototype(proto) || !proto.Components.ContainsKey(bodyName))
                         continue;
 
                     var mob = entMan.SpawnEntity(proto.ID, map.GridCoords);
