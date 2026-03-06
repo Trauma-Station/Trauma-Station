b5f092b3064d8458069f56080de8307eadd048a2
No Elka <no.elka.the.god@gmail.com>
2026-03-06 13:04:17 +0300

    o

--- Content.Server/_DV/CosmicCult/EntitySystems/CosmicRiftSystem.cs
+++ Content.Server/_DV/CosmicCult/EntitySystems/CosmicRiftSystem.cs
@@ -90,8 +90,7 @@ public sealed class CosmicRiftSystem : EntitySystem
 
     private void OnInteract(Entity<CosmicMalignRiftComponent> uid, ref InteractHandEvent args)
     {
-        if (args.Handled
-            || uid.Comp.Occupied)
+        if (args.Handled)
         {
             _popup.PopupEntity(Loc.GetString("cosmiccult-rift-inuse"), args.User, args.User);
             return;
@@ -110,7 +109,6 @@ public sealed class CosmicRiftSystem : EntitySystem
         }
 
         args.Handled = true;
-        uid.Comp.Occupied = true;
         _popup.PopupEntity(Loc.GetString("cosmiccult-rift-beginabsorb"), args.User, args.User);
         var doargs = new DoAfterArgs(EntityManager,
             args.User,
@@ -126,8 +124,7 @@ public sealed class CosmicRiftSystem : EntitySystem
 
     private void OnInteractUsing(Entity<CosmicMalignRiftComponent> uid, ref InteractUsingEvent args)
     {
-        if (args.Handled
-            || uid.Comp.Occupied)
+        if (args.Handled)
         {
             _popup.PopupEntity(Loc.GetString("cosmiccult-rift-inuse"), args.User, args.User);
             return;
@@ -135,7 +132,6 @@ public sealed class CosmicRiftSystem : EntitySystem
 
         if (HasComp<BibleComponent>(args.Used))
         {
-            uid.Comp.Occupied = true;
             _popup.PopupEntity(Loc.GetString("cosmiccult-rift-beginpurge"), args.User, args.User);
             var doargs = new DoAfterArgs(EntityManager,
                 args.User,
@@ -191,10 +187,7 @@ public sealed class CosmicRiftSystem : EntitySystem
     private void OnPurgeDoAfter(Entity<CosmicMalignRiftComponent> uid, ref EventPurgeRiftDoAfter args)
     {
         if (args.Args.Target == null || args.Cancelled || args.Handled)
-        {
-            uid.Comp.Occupied = false;
             return;
-        }
 
         args.Handled = true;
         var tgtpos = Transform(uid).Coordinates;
