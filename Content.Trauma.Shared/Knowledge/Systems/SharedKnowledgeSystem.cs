277d7eb72d40612fb030236ec4623cb74d40bde6
deltanedas <@deltanedas:kde.org>
2026-03-06 23:01:27 +0000

    disable xp gain

--- Content.Trauma.Shared/Knowledge/Systems/SharedKnowledgeSystem.cs
+++ Content.Trauma.Shared/Knowledge/Systems/SharedKnowledgeSystem.cs
@@ -222,6 +222,7 @@ public abstract partial class SharedKnowledgeSystem : CommonKnowledgeSystem
 
     public void AddExperience(Entity<KnowledgeContainerComponent> ent, [ForbidLiteral] EntProtoId id, int xp, bool popup = true)
     {
+        /* FIXME: xp gaining needs to be reworked to be less shit, each source needs to say the mastery level it can raise up to
         if (GetKnowledge(ent, id) is not {} unit)
         {
             // if you don't have it, you have a small change to learn it when gaining some xp
@@ -237,6 +238,7 @@ public abstract partial class SharedKnowledgeSystem : CommonKnowledgeSystem
             var updateEv = new UpdateExperienceEvent();
             RaiseLocalEvent(holder, ref updateEv);
         }
+        */
     }
 
     public void AddExperience(Entity<KnowledgeComponent> ent, EntityUid target, int added)
