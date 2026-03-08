fc682c4037d3878a24f1919b8716dad1dc744b20
assripper. <130258577+caoimhan2008@users.noreply.github.com>
2026-03-08 15:13:54 +0000

    useless

--- Content.Medical.Server/Abductor/AbductorSystem.Actions.cs
+++ Content.Medical.Server/Abductor/AbductorSystem.Actions.cs
@@ -145,13 +145,6 @@ public sealed partial class AbductorSystem : SharedAbductorSystem
             break;
         }
 
-        if (padFound == default)
-        {
-            _popup.PopupEntity(Loc.GetString("abductor-send-agent-no-pad"), user, user);
-            ev.Handled = true;
-            return;
-        }
-
         if (!TryComp<StrapComponent>(padFound, out var strap) || strap.BuckledEntities.Count == 0)
         {
             _popup.PopupEntity(Loc.GetString("abductor-send-agent-not-buckled"), user, user);
