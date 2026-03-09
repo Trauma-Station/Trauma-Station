3345332c72309e3b7dd9d01cd4538d18d8f7ebf1
caoimhan2008 <caoimhanmcwilliams@gmail.com>
2026-03-09 19:20:35 +0000

    timer

--- Content.Goobstation.Shared/Mindcontrol/MindcontrolledComponent.cs
+++ Content.Goobstation.Shared/Mindcontrol/MindcontrolledComponent.cs
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
 using Content.Shared.StatusIcon;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
-
 namespace Content.Goobstation.Shared.Mindcontrol;
-
 [RegisterComponent, NetworkedComponent]
 public sealed partial class MindcontrolledComponent : Component
 {
@@ -16,7 +13,8 @@ public sealed partial class MindcontrolledComponent : Component
     public SoundSpecifier MindcontrolStartSound = new SoundPathSpecifier("/Audio/_Goobstation/Ambience/Antag/mindcontrol_start.ogg");
     [DataField]
     public bool BriefingSent = false;
-
     [DataField, ViewVariables(VVAccess.ReadOnly)]
     public ProtoId<FactionIconPrototype> MindcontrolIcon { get; set; } = "MindcontrolledFaction";
+    [DataField]
+    public TimeSpan? ExpiresAt;
 }
--- Content.Trauma.Server/Abductor/AbductorMindControlSystem.cs
+++ Content.Trauma.Server/Abductor/AbductorMindControlSystem.cs
@@ -3,13 +3,15 @@
 using Content.Goobstation.Server.Mindcontrol;
 using Content.Goobstation.Shared.Mindcontrol;
 using Content.Medical.Shared.Abductor;
-using Robust.Shared.IoC;
+using Content.Shared.Mindshield.Components;
+using Robust.Shared.Timing;
 
 namespace Content.Trauma.Server.Abductor;
 
 public sealed partial class AbductorMindControlSystem : EntitySystem
 {
     [Dependency] private readonly MindcontrolSystem _mindcontrol = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
 
     public override void Initialize()
     {
@@ -17,13 +19,35 @@ public sealed partial class AbductorMindControlSystem : EntitySystem
         SubscribeLocalEvent<AbductorGizmoComponent, AbductorGizmoMindControlDoAfterEvent>(OnGizmoMindControlDoAfter);
     }
 
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<MindcontrolledComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (comp.ExpiresAt == null)
+                continue;
+
+            if (_timing.CurTime >= comp.ExpiresAt.Value)
+                RemComp<MindcontrolledComponent>(uid);
+        }
+    }
+
     private void OnGizmoMindControlDoAfter(Entity<AbductorGizmoComponent> ent, ref AbductorGizmoMindControlDoAfterEvent args)
     {
         if (args.Handled || args.Cancelled || args.Target is not {} target)
             return;
 
+        if (HasComp<MindShieldComponent>(target))
+        {
+            args.Handled = true;
+            return;
+        }
+
         var comp = EnsureComp<MindcontrolledComponent>(target);
         comp.Master = args.User;
+        comp.ExpiresAt = _timing.CurTime + TimeSpan.FromMinutes(15);
         _mindcontrol.Start(target, comp);
 
         args.Handled = true;
