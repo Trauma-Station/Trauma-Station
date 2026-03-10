44494b1745462ec972e33eb3f598449e3209b58c
deltanedas <@deltanedas:kde.org>
2026-03-10 11:34:20 +0000

    clean up leftover rev language slop

---
+++ Content.Shared/Revolutionary/SharedRevolutionarySystem.Trauma.cs
@@ -0,0 +1,15 @@
+namespace Content.Shared.Revolutionary;
+
+/// <Trauma>
+/// Trauma - goob rev mindshield conversion changes
+/// </Trauma>
+public abstract partial class SharedRevolutionarySystem
+{
+    /// <summary>
+    /// Change headrevs ability to convert people
+    /// </summary>
+    public void SetConvertAbility(Entity<HeadRevolutionaryComponent> headRev, bool enabled = true)
+    {
+        headRev.Comp.ConvertAbilityEnabled = enabled;
+    }
+}
