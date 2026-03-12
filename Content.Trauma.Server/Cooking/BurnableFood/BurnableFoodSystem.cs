d3a46102aace1a59c169adb3edf23769ce4a3851
Armok <155400926+ARMOKS@users.noreply.github.com>
2026-03-12 15:10:28 -0600

    s

--- Content.Trauma.Server/Cooking/BurnableFood/BurnableFoodSystem.cs
+++ Content.Trauma.Server/Cooking/BurnableFood/BurnableFoodSystem.cs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Shared.Popups;
 using Content.Server.Temperature.Components;
+using Content.Shared.Popups;
 using Content.Shared.Temperature;
 using Content.Trauma.Shared.BurnableFood;
 
