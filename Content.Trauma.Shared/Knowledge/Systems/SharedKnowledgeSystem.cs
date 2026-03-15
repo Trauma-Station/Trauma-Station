9ac323934dc99c18658f37a640fbb3c4f3b69ac9
Armok <155400926+ARMOKS@users.noreply.github.com>
2026-03-14 22:48:11 -0600

    s

--- Content.Trauma.Shared/Knowledge/Systems/SharedKnowledgeSystem.cs
+++ Content.Trauma.Shared/Knowledge/Systems/SharedKnowledgeSystem.cs
@@ -628,8 +628,8 @@ public abstract partial class SharedKnowledgeSystem : CommonKnowledgeSystem
             >= 76 => 4,
             >= 51 => 3,
             >= 26 => 2,
-            >= 6 => 1,
-            >= 1 => 0,
+            >= 1 => 1,
+            _ => 0,
         };
 
     public override int GetMastery(EntityUid uid)
