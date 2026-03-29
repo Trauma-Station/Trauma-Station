c39aea0721f37f9464e7c1849d9a899a15a014ab
deltanedas <@deltanedas:kde.org>
2026-03-29 15:04:31 +0100

    !

--- Content.Shared/Implants/SharedSubdermalImplantSystem.Relays.cs
+++ Content.Shared/Implants/SharedSubdermalImplantSystem.Relays.cs
@@ -25,7 +25,7 @@ public abstract partial class SharedSubdermalImplantSystem
     /// <summary>
     /// Relays events from the implanted to the implant.
     /// </summary>
-    private void RelayToImplantEvent<T>(EntityUid uid, ImplantedComponent component, T args) where T : notnull
+    public void RelayToImplantEvent<T>(EntityUid uid, ImplantedComponent component, T args) where T : notnull // Trauma - made public
     {
         if (!_container.TryGetContainer(uid, ImplanterComponent.ImplantSlotId, out var implantContainer))
             return;
