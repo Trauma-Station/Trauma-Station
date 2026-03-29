02d8203b605deb68f9d05e52d21bab86c618fd03
Armok <155400926+ARMOKS@users.noreply.github.com>
2026-03-29 13:17:14 -0600

    prices + yaml + RSI + requested

--- Content.Trauma.Server/ChangeOnCool/ChangeOnCoolSystem.cs
+++ Content.Trauma.Server/ChangeOnCool/ChangeOnCoolSystem.cs
@@ -5,6 +5,9 @@ using Content.Shared.Popups;
 using Content.Shared.Temperature;
 using Content.Trauma.Shared.ChangeOnCool;
 using NetCord.Gateway.Voice;
+
+namespace Content.Trauma.Server.ChangeOnCool;
+
 public sealed partial class ChangeOnCoolSystem : EntitySystem
 {
 
