3e49639bbfb7ad86498e6650433810adef712aa6
deltanedas <@deltanedas:kde.org>
2026-04-05 13:42:39 +0100

    late arrivals

---
+++ Content.Trauma.Shared/EntityConditions/SpawnPointHasJobCondition.cs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.EntityConditions;
+using Robust.Shared.Prototypes;
+
+namespace Content.Trauma.Shared.EntityConditions;
+
+/// <summary>
+/// Condition that requires the target player spawn point has a non-null <c>Job</c> set.
+/// </summary>
+public sealed partial class SpawnPointHasJob : EntityConditionBase<SpawnPointHasJob>
+{
+    public override string EntityConditionGuidebookText(IPrototypeManager prototype)
+        => string.Empty;
+}
