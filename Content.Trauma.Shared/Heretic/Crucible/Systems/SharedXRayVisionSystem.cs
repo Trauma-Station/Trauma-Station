43a174b84cdb5dbc02134801c00a22ec7a517a6d
Aviu00 <aviu00@protonmail.com>
2026-04-10 09:08:09 +0300

    Fix.

--- Content.Trauma.Shared/Heretic/Crucible/Systems/SharedXRayVisionSystem.cs
+++ Content.Trauma.Shared/Heretic/Crucible/Systems/SharedXRayVisionSystem.cs
@@ -49,7 +49,7 @@ public abstract class SharedXRayVisionSystem : EntitySystem
             return;
 
         if (_status.TryEffectsWithComp<XRayVisionStatusEffectComponent>(args.Target, out var effects))
-            effects.Remove(ent);
+            effects.Remove(ent.Owner);
 
         if (_timing.IsFirstTimePredicted)
             ent.Comp.ShouldRemoveFov = eye.DrawFov && effects is not { Count: > 0 };
