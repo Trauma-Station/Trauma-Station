48d53a108836ffb2b97964d0db1220a8f31c977e
deltanedas <@deltanedas:kde.org>
2026-04-14 20:30:27 +0100

    !

--- Content.Trauma.Server/Antag/AntagSummonerSystem.cs
+++ Content.Trauma.Server/Antag/AntagSummonerSystem.cs
@@ -29,7 +29,7 @@ public sealed class AntagSummonerSystem : SharedAntagSummonerSystem
         // TODO: % alive check too if people are extra chuddy
         if (_player.PlayerCount < _minPlayers)
         {
-            Popup.PopupEntity("Security grants are temporarily unavailable, please try again later.", ent, user, PopupType.SmallCaution)
+            Popup.PopupEntity("Security grants are temporarily unavailable, please try again later.", ent, user, PopupType.SmallCaution);
             return false;
         }
 
