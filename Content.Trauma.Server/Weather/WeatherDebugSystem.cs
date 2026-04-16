ec867873d4a9f51bf04acac29e826f63219c0a3a
deltanedas <@deltanedas:kde.org>
2026-04-16 15:18:55 +0100

    add debug info for ashfall

---
+++ Content.Trauma.Server/Weather/WeatherDebugSystem.cs
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Trauma.Server.Weather;
+
+// TODO: delete this after it's fixed
+public sealed class WeatherDebugSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<WeatherDebugComponent, MoveEvent>(OnMove);
+    }
+
+    private void OnMove(Entity<WeatherDebugComponent> ent, ref MoveEvent args)
+    {
+        Log.Error($"Weather {ToPrettyString(ent)} moved from {args.OldPosition} to {args.NewPosition}!\nParent: {ToPrettyString(args.Entity.Comp1)}, parent changed: {args.ParentChanged}\nStack trace: {Environment.StackTrace}");
+    }
+}
+
+[RegisterComponent]
+public sealed class WeatherDebugComponent : Component;
