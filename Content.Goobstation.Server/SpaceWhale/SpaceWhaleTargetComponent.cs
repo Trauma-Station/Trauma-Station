NameLunar/space_station_ADT

Улучшение левиафана (#2440)
## Описание PR
<!-- Что вы изменили в этом пулл-реквесте? -->
Переписал системы, убрал губовский щиткод. Теперь хвост у сущностей с
TailedEntityComponent выглядит более физично

## Почему / Баланс
<!-- Почему оно было изменено и как изменение повлияет на игру и её
баланс.

В случае, если ваш pull request привязан к запросу из нашего discord
сервера, используйте образец, представленный далее.

Ссылка на заказ, предложение или баг-репорт
- [Баг-репорт/Заказ/Предложение](ссылка)-->

## Техническая информация
<!-- Если речь идет об изменении кода, кратко изложите на высоком уровне
принцип работы нового кода. Перечислите все критические изменения,
включая изменения пространства имён, публичных классов/методов/полей-
-->
- [x] Изменения были протестированы на локальном сервере, и всё работает
отлично.
- [x] PR закончен и требует просмотра изменений.

## Медиа
<!--Вставьте медиа, демонстрирующее изменения, если это требуется-->
<img width="841" height="805" alt="image"
src="https://github.com/user-attachments/assets/6694ea24-a691-4266-bf48-1c69e5ee3fd4"
/>

## Чейнджлог

:cl: kote
- tweak: Все космические киты с инсультом вымерли. Остались только
здоровые, с подверженным физике и здравому смыслу телами.
- tweak: Космические киты больше не инвалиды и теперь двигаются в
несколько раз быстрее.

Content.Server/ADT/SpaceWhales/Components/SpaceWhaleTargetComponent.cs
@@ -1,3 +1,5 @@
+using Content.Server.ADT.MobCaller;
+
 namespace Content.Server.ADT.SpaceWhale;
 
 /// <summary>
@@ -6,5 +8,6 @@ namespace Content.Server.ADT.SpaceWhale;
 [RegisterComponent]
 public sealed partial class SpaceWhaleTargetComponent : Component
 {
-    [DataField] public EntityUid Entity { get; set; }
+    [ViewVariables(VVAccess.ReadWrite)]
+    public Entity<MobCallerComponent>? MobCaller;
 }

Content.Server/ADT/SpaceWhales/Systems/StationProximitySystem.cs
@@ -13,11 +13,11 @@ using Content.Shared.ADT.CCVar;
 using Content.Server.ADT.MobCaller;
 using System.Linq;
 using Robust.Shared.Spawners;
+using Content.Shared.Movement.Components;
+using Content.Shared.Movement.Systems;
 
 namespace Content.Server.ADT.SpaceWhale.StationProximity;
 
-// used by space whales so think twice beofre using it for yourself somewhere else
-// also half of this was taken from wizden #30436 and redone for whale purposes
 public sealed class StationProximitySystem : EntitySystem
 {
     [Dependency] private readonly IGameTiming _timing = default!;
@@ -25,18 +25,21 @@ public sealed class StationProximitySystem : EntitySystem
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly MovementSpeedModifierSystem _moveSpeed = default!;
 
-    private static readonly TimeSpan CheckInterval = TimeSpan.FromSeconds(60); // le hardcode major
+    private const float CheckInterval = 60;
     private TimeSpan _nextCheck = TimeSpan.Zero;
 
     private EntityUid? _mobCaller;
+    private bool _spawned = false;
 
     public override void Initialize()
     {
         base.Initialize();
-        _nextCheck = _timing.CurTime + CheckInterval;
+        _nextCheck = _timing.CurTime + TimeSpan.FromSeconds(CheckInterval);
 
         SubscribeLocalEvent<SpaceWhaleTargetComponent, MobStateChangedEvent>(OnTargetDeath);
+        SubscribeLocalEvent<SpaceWhaleTargetComponent, ComponentShutdown>(OnTargetShutdown);
     }
 
     private void OnTargetDeath(Entity<SpaceWhaleTargetComponent> ent, ref MobStateChangedEvent args)
@@ -44,23 +47,58 @@ public sealed class StationProximitySystem : EntitySystem
         if (args.NewMobState == MobState.Alive)
             return;
 
-        if (TryComp<MobCallerComponent>(ent.Comp.Entity, out var caller))
+        RemComp<SpaceWhaleTargetComponent>(ent.Owner);
+    }
+
+    private void OnTargetShutdown(Entity<SpaceWhaleTargetComponent> ent, ref ComponentShutdown args)
+    {
+        StopFollowing(ent.Owner);
+    }
+
+    private void StopFollowing(Entity<SpaceWhaleTargetComponent?> ent)
+    {
+        if (!Resolve(ent.Owner, ref ent.Comp, false))
+            return;
+
+        if (TryComp<MobCallerComponent>(ent.Comp.MobCaller, out var caller))
         {
             foreach (var item in caller.SpawnedEntities)
-                EnsureComp<TimedDespawnComponent>(item).Lifetime = 20f;
+            {
+                EnsureComp<TimedDespawnComponent>(item).Lifetime = 15f;
+                _moveSpeed.ChangeBaseSpeed(item, 11, 30, 1);
+                _moveSpeed.RefreshMovementSpeedModifiers(item);
+            }
         }
-        QueueDel(ent.Comp.Entity);
-        RemComp<SpaceWhaleTargetComponent>(ent.Owner);
+
+        QueueDel(ent.Comp.MobCaller);
+        _spawned = false;
     }
 
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
 
+        var query = EntityQueryEnumerator<SpaceWhaleTargetComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (!comp.MobCaller.HasValue)
+            {
+                RemCompDeferred(uid, comp);
+                continue;
+            }
+
+            var caller = comp.MobCaller.Value.Comp;
+            if (caller.SpawnedEntities.Count > 0)
+            {
+                _spawned = true;
+                break;
+            }
+        }
+
         if (_timing.CurTime < _nextCheck)
             return;
 
-        _nextCheck = _timing.CurTime + CheckInterval;
+        _nextCheck = _timing.CurTime + TimeSpan.FromSeconds(CheckInterval);
         CheckStationProximity();
     }
 
@@ -70,12 +108,12 @@ public sealed class StationProximitySystem : EntitySystem
             return;
 
         var stationQuery = EntityQueryEnumerator<BecomesStationComponent, MapGridComponent>();
-        var stations = new List<(EntityUid Uid, MapGridComponent Grid, TransformComponent Xform)>();
+        var stations = new Dictionary<EntityUid, (MapGridComponent Grid, TransformComponent Xform)>();
 
         while (stationQuery.MoveNext(out var uid, out _, out var grid))
         {
             var xform = Transform(uid);
-            stations.Add((uid, grid, xform));
+            stations.Add(uid, (grid, xform));
         }
 
         if (stations.Count == 0)
@@ -88,14 +126,15 @@ public sealed class StationProximitySystem : EntitySystem
                 continue;
 
             var sameMap = false;
-            foreach (var (_, _, stationXform) in stations)
+            foreach (var (_, (_, stationXform)) in stations)
             {
-                if (stationXform.MapUid == humanoidXform.MapUid)
-                {
-                    sameMap = true;
-                    break;
-                }
+                if (stationXform.MapUid != humanoidXform.MapUid)
+                    continue;
+
+                sameMap = true;
+                break;
             }
+
             if (!sameMap)
                 continue;
 
@@ -104,71 +143,44 @@ public sealed class StationProximitySystem : EntitySystem
     }
 
     private void CheckHumanoidProximity(EntityUid humanoid,
-        List<(EntityUid Uid, MapGridComponent Grid, TransformComponent Xform)> stations,
+        Dictionary<EntityUid, (MapGridComponent Grid, TransformComponent Xform)> stations,
         TransformComponent humanoidTransform)
     {
-        var isNearStation = false;
-
-        if (humanoidTransform.GridUid != null)
+        if (humanoidTransform.GridUid.HasValue && stations.TryGetValue(humanoidTransform.GridUid.Value, out _))
         {
-            foreach (var (stationUid, _, _) in stations)
-            {
-                if (stationUid == humanoidTransform.GridUid)
-                {
-                    isNearStation = true;
-                    break;
-                }
-            }
+            RemComp<SpaceWhaleTargetComponent>(humanoid);
+            return;
         }
 
-        if (!isNearStation) // if not, check the distance #30436
+        var humanoidWorldPos = _transform.GetWorldPosition(humanoidTransform);
+        var closestDistance = float.MaxValue;
+
+        foreach (var (stationUid, (grid, stationXform)) in stations)
         {
-            var humanoidWorldPos = _transform.GetWorldPosition(humanoidTransform);
-            var closestDistance = float.MaxValue;
+            if (stationXform.MapUid != humanoidTransform.MapUid)
+                continue;
 
-            foreach (var (stationUid, grid, stationXform) in stations)
+            var stationWorldPos = _transform.GetWorldPosition(stationXform);
+            var distance = (humanoidWorldPos - stationWorldPos).Length();
+
+            if (grid.LocalAABB.Size.Length() > 0)
             {
-                if (stationXform.MapUid != humanoidTransform.MapUid)
-                    continue;
-
-                var stationWorldPos = _transform.GetWorldPosition(stationXform);
-                var distance = (humanoidWorldPos - stationWorldPos).Length();
-
-                if (grid.LocalAABB.Size.Length() > 0)
-                {
-                    var gridRadius = grid.LocalAABB.Size.Length() / 2f; // it needs to be halved to get correct mesurements
-                    distance = Math.Max(0, distance - gridRadius);
-                }
-
-                closestDistance = Math.Min(closestDistance, distance);
+                var gridRadius = grid.LocalAABB.Size.Length() / 2f; // it needs to be halved to get correct mesurements
+                distance = Math.Max(0, distance - gridRadius);
             }
 
-            // grab the  max distance from cvar
-            isNearStation = closestDistance <= _cfg.GetCVar(ADTCCVars.SpaceWhaleSpawnDistance);
+            closestDistance = Math.Min(closestDistance, distance);
         }
 
-        if (isNearStation)
-        {
-            // if near station, remove the tracking component and delete the dummy entity
-            if (TryComp<SpaceWhaleTargetComponent>(humanoid, out var whaleTarget))
-            {
-                if (TryComp<MobCallerComponent>(whaleTarget.Entity, out var caller))
-                {
-                    foreach (var item in caller.SpawnedEntities)
-                        EnsureComp<TimedDespawnComponent>(item).Lifetime = 20f;
-                }
-
-                QueueDel(whaleTarget.Entity);
-                RemComp<SpaceWhaleTargetComponent>(humanoid);
-            }
-        }
+        if (closestDistance <= _cfg.GetCVar(ADTCCVars.SpaceWhaleSpawnDistance))
+            RemCompDeferred<SpaceWhaleTargetComponent>(humanoid);
         else
             HandleFarFromStation(humanoid);
     }
 
     private void HandleFarFromStation(EntityUid entity) // basically handles space whale spawnings
     {
-        if (EntityManager.AllEntities<SpaceWhaleTargetComponent>().Count() > 0)
+        if (_spawned)
             return;
 
         _popup.PopupEntity(
@@ -187,16 +199,20 @@ public sealed class StationProximitySystem : EntitySystem
         // Spawn a dummy entity at the player's location and lock it onto the player
         _mobCaller = Spawn(null, Transform(entity).Coordinates);
         _transform.SetParent(_mobCaller.Value, entity);
-        var mobCaller = EnsureComp<MobCallerComponent>(_mobCaller.Value); // assign the goidacaller to the dummy
+        var mobCaller = new MobCallerComponent()
+        {
 
-        mobCaller.SpawnProto = "ADTSpaceLeviathan";
-        mobCaller.MaxAlive = 1; // nuh uh
-        mobCaller.MinDistance = 100f; // should be far away
-        mobCaller.NeedAnchored = false;
-        mobCaller.NeedPower = false;
-        mobCaller.SpawnSpacing = TimeSpan.FromSeconds(30);
+            SpawnProto = "ADTSpaceLeviathan",
+            MaxAlive = 1,
+            NeedAnchored = false,
+            NeedPower = false,
+            MinDistance = 100f,
+            SpawnSpacing = TimeSpan.FromSeconds(30),
+        };
+
+        AddComp(_mobCaller.Value, mobCaller);
 
         var targetComp = EnsureComp<SpaceWhaleTargetComponent>(entity);// track the dummy on the player
-        targetComp.Entity = _mobCaller.Value;
+        targetComp.MobCaller = (_mobCaller.Value, mobCaller);
     }
 }

Content.Shared/ADT/SpaceWhales/SharedTailedEntitySystem.cs
@@ -1,0 +1,65 @@
+using System.Numerics;
+using Robust.Shared.Maths;
+using Robust.Shared.Map;
+using Robust.Shared.Network;
+using Robust.Shared.Physics;
+
+namespace Content.Shared.ADT.SpaceWhale;
+
+public abstract class SharedTailedEntitySystem : EntitySystem
+{
+    [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
+    [Dependency] private readonly INetManager _net = default!;
+
+    protected void UpdateTailPositions(Entity<TailedEntityComponent, TransformComponent> ent, float frameTime)
+    {
+        var (uid, comp, xform) = ent;
+
+        // Use the head's world position to determine whether anything moved.
+        var headPos = _transformSystem.GetWorldPosition(xform);
+        if (headPos == comp.LastPos)
+            return;
+
+        for (var i = 0; i < comp.TailSegments.Count; i++)
+        {
+            if (!TryGetEntity(comp.TailSegments[i], out var segment))
+                continue;
+
+            EntityUid? next = null;
+
+            if (i <= 0)
+                next = uid;
+            else
+                TryGetEntity(comp.TailSegments[i - 1], out next);
+
+            if (!next.HasValue)
+                continue;
+
+            var segPos = _transformSystem.GetWorldPosition(segment.Value);
+            var nextPos = _transformSystem.GetWorldPosition(next.Value);
+            var nextRot = Angle.FromWorldVec(_transformSystem.GetWorldPosition(next.Value) - segPos);
+
+            // Compute the desired position: keep `Spacing` units behind the next entity along the line
+            // from the segment to the next entity. If the segment is exactly on top of the target, fall back
+            // to using the target's forward vector.
+            var toTarget = nextPos - segPos;
+            var distance = toTarget.Length();
+
+            Vector2 desiredPos = Vector2.Zero;
+            if (distance > 0.0001f)
+            {
+                var dir = toTarget / distance;
+                desiredPos = nextPos - dir * comp.Spacing;
+            }
+            else
+            {
+                desiredPos = nextPos - nextRot.ToWorldVec() * comp.Spacing;
+            }
+
+            // Server remains authoritative and snaps to maintain exact spacing.
+            _transformSystem.SetWorldPositionRotation(segment.Value, desiredPos, nextRot);
+        }
+
+        comp.LastPos = headPos;
+    }
+}

Trauma-Station/Trauma-Station

Content.Goobstation.Server/SpaceWhale/SpaceWhaleTargetComponent.cs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Goobstation.Server.MobCaller;
+
 namespace Content.Goobstation.Server.SpaceWhale;
 
 /// <summary>
@@ -8,5 +10,6 @@ namespace Content.Goobstation.Server.SpaceWhale;
 [RegisterComponent]
 public sealed partial class SpaceWhaleTargetComponent : Component
 {
-    [DataField] public EntityUid Entity { get; set; }
+    [ViewVariables(VVAccess.ReadWrite)]
+    public Entity<MobCallerComponent>? MobCaller;
 }

Content.Goobstation.Server/SpaceWhale/StationProximity/StationProximitySystem.cs
@@ -13,11 +13,11 @@ using Robust.Shared.Audio;
 using Robust.Shared.Configuration;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Timing;
+using Robust.Shared.Spawners;
+using Content.Shared.Movement.Systems;
 
 namespace Content.Goobstation.Server.SpaceWhale.StationProximity;
 
-// used by space whales so think twice beofre using it for yourself somewhere else
-// also half of this was taken from wizden #30436 and redone for whale purposes
 public sealed class StationProximitySystem : EntitySystem
 {
     [Dependency] private readonly IGameTiming _timing = default!;
@@ -25,24 +25,80 @@ public sealed class StationProximitySystem : EntitySystem
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly MovementSpeedModifierSystem _moveSpeed = default!;
 
-    private static readonly TimeSpan CheckInterval = TimeSpan.FromSeconds(60); // le hardcode major
+    private const float CheckInterval = 60;
     private TimeSpan _nextCheck = TimeSpan.Zero;
+
+    private EntityUid? _mobCaller;
+    private bool _spawned = false;
 
     public override void Initialize()
     {
         base.Initialize();
-        _nextCheck = _timing.CurTime + CheckInterval;
+        _nextCheck = _timing.CurTime + TimeSpan.FromSeconds(CheckInterval);
+
+        SubscribeLocalEvent<SpaceWhaleTargetComponent, MobStateChangedEvent>(OnTargetDeath);
+        SubscribeLocalEvent<SpaceWhaleTargetComponent, ComponentShutdown>(OnTargetShutdown);
+    }
+
+    private void OnTargetDeath(Entity<SpaceWhaleTargetComponent> ent, ref MobStateChangedEvent args)
+    {
+        if (args.NewMobState == MobState.Alive)
+            return;
+
+        RemComp<SpaceWhaleTargetComponent>(ent.Owner);
+    }
+
+    private void OnTargetShutdown(Entity<SpaceWhaleTargetComponent> ent, ref ComponentShutdown args)
+    {
+        StopFollowing(ent.Owner);
+    }
+
+    private void StopFollowing(Entity<SpaceWhaleTargetComponent?> ent)
+    {
+        if (!Resolve(ent.Owner, ref ent.Comp, false))
+            return;
+
+        if (TryComp<MobCallerComponent>(ent.Comp.MobCaller, out var caller))
+        {
+            foreach (var item in caller.SpawnedEntities)
+            {
+                EnsureComp<TimedDespawnComponent>(item).Lifetime = 15f;
+                _moveSpeed.ChangeBaseSpeed(item, 11, 30, 1);
+                _moveSpeed.RefreshMovementSpeedModifiers(item);
+            }
+        }
+
+        QueueDel(ent.Comp.MobCaller);
+        _spawned = false;
     }
 
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
 
+        var query = EntityQueryEnumerator<SpaceWhaleTargetComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (!comp.MobCaller.HasValue)
+            {
+                RemCompDeferred(uid, comp);
+                continue;
+            }
+
+            var caller = comp.MobCaller.Value.Comp;
+            if (caller.SpawnedEntities.Count > 0)
+            {
+                _spawned = true;
+                break;
+            }
+        }
+
         if (_timing.CurTime < _nextCheck)
             return;
 
-        _nextCheck = _timing.CurTime + CheckInterval;
+        _nextCheck = _timing.CurTime + TimeSpan.FromSeconds(CheckInterval);
         CheckStationProximity();
     }
 
@@ -52,12 +108,12 @@ public sealed class StationProximitySystem : EntitySystem
             return;
 
         var stationQuery = EntityQueryEnumerator<BecomesStationComponent, MapGridComponent>();
-        var stations = new List<(EntityUid Uid, MapGridComponent Grid, TransformComponent Xform)>();
+        var stations = new Dictionary<EntityUid, (MapGridComponent Grid, TransformComponent Xform)>();
 
         while (stationQuery.MoveNext(out var uid, out _, out var grid))
         {
             var xform = Transform(uid);
-            stations.Add((uid, grid, xform));
+            stations.Add(uid, (grid, xform));
         }
 
         if (stations.Count == 0)
@@ -70,14 +126,15 @@ public sealed class StationProximitySystem : EntitySystem
                 continue;
 
             var sameMap = false;
-            foreach (var (_, _, stationXform) in stations)
+            foreach (var (_, (_, stationXform)) in stations)
             {
-                if (stationXform.MapUid == humanoidXform.MapUid)
-                {
-                    sameMap = true;
-                    break;
-                }
+                if (stationXform.MapUid != humanoidXform.MapUid)
+                    continue;
+
+                sameMap = true;
+                break;
             }
+
             if (!sameMap)
                 continue;
 
@@ -86,65 +143,44 @@ public sealed class StationProximitySystem : EntitySystem
     }
 
     private void CheckHumanoidProximity(EntityUid humanoid,
-        List<(EntityUid Uid, MapGridComponent Grid, TransformComponent Xform)> stations,
+        Dictionary<EntityUid, (MapGridComponent Grid, TransformComponent Xform)> stations,
         TransformComponent humanoidTransform)
     {
-        var isNearStation = false;
-
-        if (humanoidTransform.GridUid != null)
+        if (humanoidTransform.GridUid.HasValue && stations.TryGetValue(humanoidTransform.GridUid.Value, out _))
         {
-            foreach (var (stationUid, _, _) in stations)
-            {
-                if (stationUid == humanoidTransform.GridUid)
-                {
-                    isNearStation = true;
-                    break;
-                }
-            }
+            RemComp<SpaceWhaleTargetComponent>(humanoid);
+            return;
         }
 
-        if (!isNearStation) // if not, check the distance #30436
+        var humanoidWorldPos = _transform.GetWorldPosition(humanoidTransform);
+        var closestDistance = float.MaxValue;
+
+        foreach (var (stationUid, (grid, stationXform)) in stations)
         {
-            var humanoidWorldPos = _transform.GetWorldPosition(humanoidTransform);
-            var closestDistance = float.MaxValue;
+            if (stationXform.MapUid != humanoidTransform.MapUid)
+                continue;
 
-            foreach (var (stationUid, grid, stationXform) in stations)
+            var stationWorldPos = _transform.GetWorldPosition(stationXform);
+            var distance = (humanoidWorldPos - stationWorldPos).Length();
+
+            if (grid.LocalAABB.Size.Length() > 0)
             {
-                if (stationXform.MapUid != humanoidTransform.MapUid)
-                    continue;
-
-                var stationWorldPos = _transform.GetWorldPosition(stationXform);
-                var distance = (humanoidWorldPos - stationWorldPos).Length();
-
-                if (grid.LocalAABB.Size.Length() > 0)
-                {
-                    var gridRadius = grid.LocalAABB.Size.Length() / 2f; // it needs to be halved to get correct mesurements
-                    distance = Math.Max(0, distance - gridRadius);
-                }
-
-                closestDistance = Math.Min(closestDistance, distance);
+                var gridRadius = grid.LocalAABB.Size.Length() / 2f; // it needs to be halved to get correct mesurements
+                distance = Math.Max(0, distance - gridRadius);
             }
 
-            // grab the  max distance from cvar
-            isNearStation = closestDistance <= _cfg.GetCVar(GoobCVars.SpaceWhaleSpawnDistance);
+            closestDistance = Math.Min(closestDistance, distance);
         }
 
-        if (isNearStation)
-        {
-            // if near station, remove the tracking component and delete the dummy entity
-            if (TryComp<SpaceWhaleTargetComponent>(humanoid, out var whaleTarget))
-            {
-                QueueDel(whaleTarget.Entity);
-                RemComp<SpaceWhaleTargetComponent>(humanoid);
-            }
-        }
+        if (closestDistance <= _cfg.GetCVar(GoobCVars.SpaceWhaleSpawnDistance))
+            RemCompDeferred<SpaceWhaleTargetComponent>(humanoid);
         else
             HandleFarFromStation(humanoid);
     }
 
     private void HandleFarFromStation(EntityUid entity) // basically handles space whale spawnings
     {
-        if (HasComp<SpaceWhaleTargetComponent>(entity))
+        if (_spawned)
             return;
 
         _popup.PopupEntity(
@@ -159,18 +195,21 @@ public sealed class StationProximitySystem : EntitySystem
             AudioParams.Default.WithVolume(1f));
 
         // Spawn a dummy entity at the player's location and lock it onto the player
-        var dummy = Spawn(null, Transform(entity).Coordinates);
-        _transform.SetParent(dummy, entity);
-        var mobCaller = EnsureComp<MobCallerComponent>(dummy); // assign the goidacaller to the dummy
+        _mobCaller = Spawn(null, Transform(entity).Coordinates);
+        _transform.SetParent(_mobCaller.Value, entity);
+        var mobCaller = new MobCallerComponent()
+        {
+            SpawnProto = "ADTSpaceLeviathan",
+            MaxAlive = 1,
+            NeedAnchored = false,
+            NeedPower = false,
+            MinDistance = 100f,
+            SpawnSpacing = TimeSpan.FromSeconds(30),
+        };
 
-        mobCaller.SpawnProto = "SpaceLeviathanDespawn";
-        mobCaller.MaxAlive = 1; // nuh uh
-        mobCaller.MinDistance = 100f; // should be far away
-        mobCaller.NeedAnchored = false;
-        mobCaller.NeedPower = false;
-        mobCaller.SpawnSpacing = TimeSpan.FromSeconds(65); // to give the guy some time to get back to the station + prevent him from like, QSI-ing to the station to summon the worm in the station lmao, also bru these 5 seconds are really important
+        AddComp(_mobCaller.Value, mobCaller);
 
         var targetComp = EnsureComp<SpaceWhaleTargetComponent>(entity);// track the dummy on the player
-        targetComp.Entity = dummy;
+        targetComp.MobCaller = (_mobCaller.Value, mobCaller);
     }
 }

Content.Goobstation.Shared/SpaceWhale/SharedTailedEntitySystem.cs
@@ -1,0 +1,61 @@
+using System.Numerics;
+
+namespace Content.Goobstation.Shared.SpaceWhale;
+
+public abstract class SharedTailedEntitySystem : EntitySystem
+{
+    [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
+    [Dependency] private readonly INetManager _net = default!;
+
+    protected void UpdateTailPositions(Entity<TailedEntityComponent, TransformComponent> ent, float frameTime)
+    {
+        var (uid, comp, xform) = ent;
+
+        // Use the head's world position to determine whether anything moved.
+        var headPos = _transformSystem.GetWorldPosition(xform);
+        if (headPos == comp.LastPos)
+            return;
+
+        for (var i = 0; i < comp.TailSegments.Count; i++)
+        {
+            if (!TryGetEntity(comp.TailSegments[i], out var segment))
+                continue;
+
+            EntityUid? next = null;
+
+            if (i <= 0)
+                next = uid;
+            else
+                TryGetEntity(comp.TailSegments[i - 1], out next);
+
+            if (!next.HasValue)
+                continue;
+
+            var segPos = _transformSystem.GetWorldPosition(segment.Value);
+            var nextPos = _transformSystem.GetWorldPosition(next.Value);
+            var nextRot = Angle.FromWorldVec(_transformSystem.GetWorldPosition(next.Value) - segPos);
+
+            // Compute the desired position: keep `Spacing` units behind the next entity along the line
+            // from the segment to the next entity. If the segment is exactly on top of the target, fall back
+            // to using the target's forward vector.
+            var toTarget = nextPos - segPos;
+            var distance = toTarget.Length();
+
+            Vector2 desiredPos = Vector2.Zero;
+            if (distance > 0.0001f)
+            {
+                var dir = toTarget / distance;
+                desiredPos = nextPos - dir * comp.Spacing;
+            }
+            else
+            {
+                desiredPos = nextPos - nextRot.ToWorldVec() * comp.Spacing;
+            }
+
+            // Server remains authoritative and snaps to maintain exact spacing.
+            _transformSystem.SetWorldPositionRotation(segment.Value, desiredPos, nextRot);
+        }
+
+        comp.LastPos = headPos;
+    }
+}