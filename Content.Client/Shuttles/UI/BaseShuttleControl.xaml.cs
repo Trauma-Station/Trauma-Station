7dbe4bb0f227e996c935415a7946daad60a16538
Papiditel <mrharved@gmail.com>
2026-05-03 14:52:29 +0100

    Update BaseShuttleControl.xaml.cs

--- Content.Client/Shuttles/UI/BaseShuttleControl.xaml.cs
+++ Content.Client/Shuttles/UI/BaseShuttleControl.xaml.cs
@@ -47,7 +47,7 @@ public partial class BaseShuttleControl : MapGridControl
     {
         RobustXamlLoader.Load(this);
         Maps = EntManager.System<SharedMapSystem>();
-        Font = new VectorFont(IoCManager.Resolve<IResourceCache>().GetResource<FontResource>("/Fonts/_Trauma/ScienceGothic-Regular.ttf"), 12); // <Trauma>
+        Font = new VectorFont(IoCManager.Resolve<IResourceCache>().GetResource<FontResource>("/Fonts/_Trauma/ScienceGothic-Regular.ttf"), 12); // Trauma
         _drawJob = new GridDrawJob()
         {
             ScaledVertices = _allVertices,
