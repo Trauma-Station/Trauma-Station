4dd6e2ffff1ef3ec5cbcf360c77d176cba6bdb4e
Papiditel <mrharved@gmail.com>
2026-05-03 14:52:52 +0100

    Update NavMapControl.cs

--- Content.Client/Pinpointer/UI/NavMapControl.cs
+++ Content.Client/Pinpointer/UI/NavMapControl.cs
@@ -432,9 +432,7 @@ public partial class NavMapControl : MapGridControl
 
             // Calculate font size for current zoom level
             var fontSize = (int)Math.Round(1 / WorldRange * DefaultDisplayedRange * UIScale * _targetFontsize, 0);
-            // <Trauma>
-            var font = new VectorFont(_cache.GetResource<FontResource>("/Fonts/_Trauma/ScienceGothic-Bold.ttf"), fontSize);
-            // </Trauma>
+            var font = new VectorFont(_cache.GetResource<FontResource>("/Fonts/_Trauma/ScienceGothic-Bold.ttf"), fontSize); // Trauma
 
             foreach (var beacon in _navMap.Beacons.Values)
             {
