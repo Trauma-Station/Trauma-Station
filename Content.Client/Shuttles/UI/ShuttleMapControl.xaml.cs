9756bfbf1d13624f037497a6d5c3824625ad642d
Papiditel <mrharved@gmail.com>
2026-05-03 14:52:31 +0100

    Update ShuttleMapControl.xaml.cs

--- Content.Client/Shuttles/UI/ShuttleMapControl.xaml.cs
+++ Content.Client/Shuttles/UI/ShuttleMapControl.xaml.cs
@@ -80,7 +80,7 @@ public sealed partial class ShuttleMapControl : BaseShuttleControl
         var cache = IoCManager.Resolve<IResourceCache>();
 
         _physicsQuery = EntManager.GetEntityQuery<PhysicsComponent>();
-        _font = new VectorFont(cache.GetResource<FontResource>("/Fonts/_Trauma/ScienceGothic-Regular.ttf"), 10); // <Trauma>
+        _font = new VectorFont(cache.GetResource<FontResource>("/Fonts/_Trauma/ScienceGothic-Regular.ttf"), 10); // Trauma
     }
 
     public void SetMap(MapId mapId, Vector2 offset, bool recentering = false)
