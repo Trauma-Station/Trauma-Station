a5328a8fc3b1cb9afef09fde5f52767a6fb6ff1d
Lumminal <spotifylefteris@gmail.com>
2026-05-09 22:26:29 +0300

    (add) gamerule for testing

---
+++ Content.Trauma.Server/Vampires/GameTicking/VampireRuleSystem.cs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Server.GameTicking.Rules;
+
+namespace Content.Trauma.Server.Vampires.GameTicking;
+
+public sealed class VampireRuleSystem : GameRuleSystem<VampireRuleComponent>
+{
+    // TODO: Implement
+}
