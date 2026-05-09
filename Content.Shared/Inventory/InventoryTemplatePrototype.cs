045c498b79f4afb25f317b93892e840e25ba0c93
deltanedas <@deltanedas:kde.org>
2026-05-09 12:20:09 +0100

    inventory slot linting, system.numerics global using, glue/lube immunity

--- Content.Shared/Inventory/InventoryTemplatePrototype.cs
+++ Content.Shared/Inventory/InventoryTemplatePrototype.cs
@@ -1,3 +1,6 @@
+// <Trauma>
+using Content.Trauma.Common.Inventory;
+// </Trauma>
 using System.Numerics;
 using Content.Shared.Strip;
 using Content.Shared.Whitelist;
@@ -16,7 +19,7 @@ public sealed partial class InventoryTemplatePrototype : IPrototype
 [DataDefinition]
 public sealed partial class SlotDefinition
 {
-    [DataField("name", required: true)] public string Name { get; private set; } = string.Empty;
+    [DataField(required: true)] public ProtoId<InventorySlotPrototype> Name { get; private set; } // Trauma - string -> ProtoId
     [DataField("slotTexture")] public string TextureName { get; private set; } = "pocket";
     /// <summary>
     /// The texture displayed in a slot when it has an item inside of it.
