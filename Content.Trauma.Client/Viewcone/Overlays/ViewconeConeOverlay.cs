7d0237746588bd594f3df4a19b32e718fddb4033
deltanedas <@deltanedas:kde.org>
2026-05-14 19:52:50 +0100

    !

--- Content.Trauma.Client/Viewcone/Overlays/ViewconeConeOverlay.cs
+++ Content.Trauma.Client/Viewcone/Overlays/ViewconeConeOverlay.cs
@@ -87,7 +87,7 @@ public sealed partial class ViewconeConeOverlay : Overlay
         _viewconeShader.SetParameter("ConeFeather", _coneFeather);
         _viewconeShader.SetParameter("ConeIgnoreRadius", _coneIgnoreRadius);
         _viewconeShader.SetParameter("ConeIgnoreFeather", _coneIgnoreFeather);
-        _viewconeShader.SetParameter("GrainScale", _grainScale);
+        _viewconeShader.SetParameter("GrainScale", GrainScale);
 
         worldHandle.UseShader(_viewconeShader);
         worldHandle.DrawRect(viewport, Color.White);
