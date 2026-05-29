6152a8f2484ab0d163e742cc3c07592f4fd5654f
Lumminal <spotifylefteris@gmail.com>
2026-05-29 03:40:59 +0300

    holy goida

--- Content.Trauma.Shared/Vampires/Gargantua/GargantuaChargingSystem.cs
+++ Content.Trauma.Shared/Vampires/Gargantua/GargantuaChargingSystem.cs
@@ -3,13 +3,14 @@
 using Content.Shared.EntityEffects;
 using Content.Shared.Throwing;
 using Robust.Shared.Physics.Events;
+using Robust.Shared.Timing;
 
 namespace Content.Trauma.Shared.Vampires.Gargantua;
 
 public sealed partial class GargantuaChargingSystem : EntitySystem
 {
+    [Dependency] private IGameTiming _timing = default!;
     [Dependency] private SharedEntityEffectsSystem _effects = default!;
-    [Dependency] private INetManager _net = default!;
 
     public override void Initialize()
     {
@@ -17,8 +18,36 @@ public sealed partial class GargantuaChargingSystem : EntitySystem
 
         SubscribeLocalEvent<GargantuaChargingComponent, StartCollideEvent>(OnCollide);
 
+        SubscribeLocalEvent<GargantuaChargingComponent, MapInitEvent>(OnMapInit);
+
         SubscribeLocalEvent<GargantuaChargingComponent, LandEvent>(OnLand);
         SubscribeLocalEvent<GargantuaChargingComponent, StopThrowEvent>(OnStopThrow);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var now = _timing.CurTime;
+
+        var eqe = EntityQueryEnumerator<GargantuaChargingComponent>();
+        while (eqe.MoveNext(out var uid, out var comp))
+        {
+            if (comp.NextDelete < now)
+                continue;
+
+            if (TerminatingOrDeleted(uid))
+                continue;
+
+            RemCompDeferred(uid, comp);
+        }
+    }
+
+    private void OnMapInit(Entity<GargantuaChargingComponent> ent, ref MapInitEvent args)
+    {
+        // exists in case the component doesn't get deleted
+        ent.Comp.NextDelete = _timing.CurTime + ent.Comp.Delete;
+        Dirty(ent);
     }
 
     private void OnCollide(Entity<GargantuaChargingComponent> ent, ref StartCollideEvent args)
