3d74f400b091167b874e1f95d7510eb0cbbf3a39
GeneRickUsername <52056666+GeneRickUsername@users.noreply.github.com>
2026-06-01 11:07:46 -0700

    Fix

--- Content.Server/Radio/EntitySystems/RadioSystem.Trauma.cs
+++ Content.Server/Radio/EntitySystems/RadioSystem.Trauma.cs
@@ -7,6 +7,7 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 
 namespace Content.Server.Radio.EntitySystems;
+
 public sealed partial class RadioSystem
 {
     private string WrapRadioMessage(
