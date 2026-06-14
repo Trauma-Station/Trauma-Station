a0c0f995769b7b030016a84a64f91f3f3a8255eb
Aviu00 <aviu00@protonmail.com>
2026-06-14 09:37:15 +0300

    unneeded

--- Content.Trauma.Server/Heretic/Systems/PathSpecific/BladeArenaSystem.cs
+++ Content.Trauma.Server/Heretic/Systems/PathSpecific/BladeArenaSystem.cs
@@ -9,7 +9,6 @@ using Content.Server.Hands.Systems;
 using Content.Server.Roles;
 using Content.Shared.Body;
 using Content.Shared.Chat;
-using Content.Shared.Construction.EntitySystems;
 using Content.Shared.Damage.Components;
 using Content.Shared.Damage.Systems;
 using Content.Shared.Doors.Components;
@@ -63,7 +62,6 @@ public sealed partial class BladeArenaSystem : SharedBladeArenaSystem
     [Dependency] private HereticSystem _heretic = default!;
     [Dependency] private DamageableSystem _dmg = default!;
     [Dependency] private EntityWhitelistSystem _whitelist = default!;
-    [Dependency] private AnchorableSystem _anchor = default!;
 
     private readonly List<TileRef> _tilesToConvert = new();
     private readonly HashSet<Entity<AirtightComponent>> _intersecting = new();
@@ -408,7 +406,7 @@ public sealed partial class BladeArenaSystem : SharedBladeArenaSystem
     {
         var spawned = Spawn(proto, coords);
         var xform = Transform(spawned);
-        if (!xform.Anchored && _anchor.CanAnchorAt(spawned, xform.Coordinates))
+        if (!xform.Anchored)
             _transform.AnchorEntity((spawned, xform), grid);
         arena.SpawnedEntities.Add(spawned);
     }
