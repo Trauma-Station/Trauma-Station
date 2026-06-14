f01dd94eaac7b389c0cab16456564d6c8fb3cad8
deltanedas <@deltanedas:goida.zip>
2026-06-14 22:24:26 +0100

    fix mime skillchip

---
+++ Content.Trauma.Shared/Body/Chips/OrganChipComponentsComponent.cs
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Medical.Common.Body;
+
+/// <summary>
+/// Adds and removes components from the body this organ chip is installed into.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
+public sealed partial class OrganChipComponentsComponent : Component
+{
+    /// <summary>
+    /// When attached, the chip will ensure these components on the entity, and delete them on removal.
+    /// </summary>
+    [DataField]
+    public ComponentRegistry? OnAdd;
+
+    /// <summary>
+    /// When removed, the chip will ensure these components on the entity, and delete them on insertion.
+    /// </summary>
+    [DataField]
+    public ComponentRegistry? OnRemove;
+}
