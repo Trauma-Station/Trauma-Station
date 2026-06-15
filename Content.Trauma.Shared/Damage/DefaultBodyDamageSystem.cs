d21ecd98f3cecfb511a9e747888a28e6f5f78933
Aviu00 <aviu00@protonmail.com>
2026-06-15 20:21:46 +0300

    Revert "fix?"
    This reverts commit 2b30c8de741084a6a1b782d07fcbd0c08e85cbf7.

--- Content.Trauma.Shared/Damage/DefaultBodyDamageSystem.cs
+++ Content.Trauma.Shared/Damage/DefaultBodyDamageSystem.cs
@@ -3,7 +3,6 @@
 using Content.Medical.Common.Body;
 using Content.Medical.Common.Targeting;
 using Content.Shared.Body;
-using Content.Shared.Body.Systems;
 using Content.Shared.Damage.Systems;
 
 namespace Content.Trauma.Shared.Damage;
@@ -17,8 +16,7 @@ public sealed partial class DefaultBodyDamageSystem : EntitySystem
     {
         base.Initialize();
 
-        SubscribeLocalEvent<DefaultBodyDamageComponent, BodyInitEvent>(OnBodyInit,
-            after: new[] { typeof(SharedBloodstreamSystem) }); // needs the parts to exist to damage them
+        SubscribeLocalEvent<DefaultBodyDamageComponent, BodyInitEvent>(OnBodyInit); // needs the parts to exist to damage them
     }
 
     private void OnBodyInit(Entity<DefaultBodyDamageComponent> ent, ref BodyInitEvent args)
