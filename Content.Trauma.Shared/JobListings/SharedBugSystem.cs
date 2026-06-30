782fa79cd740631741206076923c5bb536e1b230
SpeltIncorrectyl <speltincorrectyl0001@gmail.com>
2026-06-30 19:49:43 +0100

    added bug area examine

--- Content.Trauma.Shared/JobListings/SharedBugSystem.cs
+++ Content.Trauma.Shared/JobListings/SharedBugSystem.cs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Shared.Examine;
+using Content.Trauma.Shared.Areas;
 
 namespace Content.Trauma.Shared.JobListings;
 
@@ -11,11 +12,26 @@ namespace Content.Trauma.Shared.JobListings;
 public abstract partial class SharedBugSystem : EntitySystem
 {
     [Dependency] protected IPrototypeManager _proto = default!;
+    [Dependency] protected AreaSystem _area =  default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<BugComponent, ExaminedEvent>(OnExamine);
+    }
+
+    /// <summmary>
+    /// Works out if the bug is in the correct area.
+    /// </summary>
+    public bool IsInCorrectArea(Entity<BugComponent> entity)
+    {
+        var area = _area.GetArea(entity.Owner);
+        if (area is null)
+            return false;
+        var prototype = MetaData(area.Value).EntityPrototype;
+        if (prototype is null)
+            return false;
+        return prototype.ID == entity.Comp.TargetArea;
     }
 
     private void OnExamine(Entity<BugComponent> entity, ref ExaminedEvent args)
@@ -24,5 +40,10 @@ public abstract partial class SharedBugSystem : EntitySystem
             return;
 
         args.PushMarkup(Loc.GetString("bug-examine-target-area", ("target-area", prototype.Name)));
+
+        if (Transform(entity.Owner).Anchored)
+        {
+            args.PushMarkup(Loc.GetString(IsInCorrectArea(entity) ? "bug-examine-correct-area" : "bug-examine-incorrect-area"));
+        }
     }
 }
--- Resources/Locale/en-US/_Trauma/job-listings/bug.ftl
+++ Resources/Locale/en-US/_Trauma/job-listings/bug.ftl
@@ -1,1 +1,3 @@
 bug-examine-target-area = This bug should be planted in the [color=darkred]{$target-area}[/color].
+bug-examine-correct-area = This bug is planted in the [color=green]correct[/color] area.
+bug-examine-incorrect-area = This bug is planted in an [color=red]incorrect[/color] area.
