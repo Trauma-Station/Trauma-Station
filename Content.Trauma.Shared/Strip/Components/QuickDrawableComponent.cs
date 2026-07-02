e8b33e939f3254cfcfcf43fd7078b8c626a4b961
Tomás Martins <tomgodmartins@gmail.com>
2026-07-02 05:11:52 +0100

    forgot this

--- Content.Trauma.Shared/Strip/Components/QuickDrawableComponent.cs
+++ Content.Trauma.Shared/Strip/Components/QuickDrawableComponent.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 /// Marks an entity's ItemSlots as eligible for the strip-system's "draw weapon" verb.
-/// Add this to sheath prototypes.
+/// Add this to sheath and holster prototypes.
 /// </summary>
 [RegisterComponent]
 public sealed partial class QuickDrawableComponent : Component;
