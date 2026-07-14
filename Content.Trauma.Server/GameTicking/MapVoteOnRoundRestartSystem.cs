4e1b42d0b73e74f916c24d015428e9dc19a4b425
No Elka <no.elka.the.god@gmail.com>
2026-07-14 11:47:18 +0300

    Map vote but good

---
+++ Content.Trauma.Server/GameTicking/MapVoteOnRoundRestartSystem.cs
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Server.GameTicking;
+using Content.Server.Voting.Managers;
+using Content.Shared.Voting;
+using Content.Trauma.Common.CCVar;
+using Robust.Shared.Configuration;
+
+namespace Content.Trauma.Server.GameTicking;
+
+/// <summary>
+/// Starts a map vote at the end of every round, if enabled in the configs
+/// </summary>
+public sealed partial class MapVoteOnRoundRestartSystem : EntitySystem
+{
+    [Dependency] private IConfigurationManager _cfg = default!;
+    [Dependency] private IVoteManager _vote = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<GameRunLevelChangedEvent>(OnRunLevelChanged);
+    }
+
+    private void OnRunLevelChanged(GameRunLevelChangedEvent args)
+    {
+        if (!_cfg.GetCVar(TraumaCVars.AutomaticMapVote)
+        || args.New != GameRunLevel.PreRoundLobby) return;
+        _vote.CreateStandardVote(null, StandardVoteType.Map);
+    }
+}
