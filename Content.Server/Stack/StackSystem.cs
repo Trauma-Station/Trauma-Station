e2f0b151ab13055c600849a4b15e770190d63ed6
riccardi48 <39341154+riccardi48@users.noreply.github.com>
2026-07-21 01:14:17 +0100

    Stack system tweaks (#44662)
    * init
    
    * oops
    
    * changed order of TryMergeStacks
    
    * cleanup
    
    * tiny optimization
    
    * documentation
    
    * clean up
    
    * popup
    
    * flip args back
    
    * bit more docs
    
    * re-added popup
    
    * move to prototype
    
    * fix
    
    * animation should always play here
    
    * update after merge conflict
    
    * review requests
    
    * small tweak
    
    * fix
    
    * Update SharedStackSystem.cs
    
    * spelling
    
    * Revert "move to prototype"
    
    * review requests
    
    * docs
    
    ---------
    
    Co-authored-by: Pok245 <113675512+Pok27@users.noreply.github.com>

--- Content.Server/Stack/StackSystem.cs
+++ Content.Server/Stack/StackSystem.cs
@@ -16,21 +16,15 @@ namespace Content.Server.Stack
     {
         [Dependency] private SharedHandsSystem _hands = default!;
         [Dependency] private SharedPopupSystem _popup = default!;
+        [Dependency] private SharedTransformSystem _transform = default!;
 
         [Dependency] private EntityQuery<StackComponent> _stackQuery;
 
         #region Spawning
 
-        /// <summary>
-        /// Spawns a new entity and moves an amount to it from the stack.
-        /// Moves nothing if amount is greater than ent's stack count.
-        /// </summary>
-        /// <param name="ent">Entity to split in a new stack.</param>
-        /// <param name="amount">How much to move to the new entity.</param>
-        /// <param name="spawnPosition">Where to spawn the new stack</param>
-        /// <returns>Null if StackComponent doesn't resolve, or amount to move is greater than ent has available.</returns>
+        /// <inheritdoc />
         [PublicAPI]
-        public override EntityUid? Split(Entity<StackComponent?> ent, int amount, EntityCoordinates spawnPosition) // Goob - override virtual method
+        public override EntityUid? Split(Entity<StackComponent?> ent, int amount, EntityCoordinates spawnPosition, EntityUid? user = null)
         {
             if (!_stackQuery.Resolve(ent.Owner, ref ent.Comp))
                 return null;
@@ -52,7 +46,7 @@ namespace Content.Server.Stack
             stackComp.Unlimited = false; // Don't let people dupe unlimited stacks
             Dirty(newEntity, stackComp);
 
-            var ev = new StackSplitEvent(newEntity);
+            var ev = new StackSplitEvent(newEntity, user);
             RaiseLocalEvent(ent, ref ev);
 
             return newEntity;
