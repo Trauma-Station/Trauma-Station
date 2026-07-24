48f6725b2b5c6e5a15fde74a9aa990f249c10448
Armok <155400926+ARMOKS@users.noreply.github.com>
2026-07-24 13:55:33 -0600

    a

--- Content.Goobstation.Shared/Sandevistan/SandevistanUserComponent.cs
+++ Content.Goobstation.Shared/Sandevistan/SandevistanUserComponent.cs
@@ -59,7 +59,7 @@ public sealed partial class SandevistanUserComponent : Component
     {
         DamageDict = new()
         {
-            { "Blunt", 6.5 },
+            { "Genetic", 6.5 },
         },
     };
 
