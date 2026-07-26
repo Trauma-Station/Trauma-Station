space-syndicate/space-station-14

cleanup all popups on client and server (#44572)
* cleanup popups on client and server

* fix RDC BUI

Content.Server/Chemistry/EntitySystems/SolutionInjectOnEventSystem.cs
@@ -10,6 +10,7 @@ using Content.Shared.Tag;
 using Content.Shared.Weapons.Melee.Events;
 using Robust.Shared.Collections;
 using Robust.Shared.Prototypes;
+using Content.Shared.IdentityManagement;
 
 namespace Content.Server.Chemistry.EntitySystems;
 
@@ -77,7 +78,7 @@ public sealed partial class SolutionInjectOnCollideSystem : EntitySystem
     /// </list>
     /// </remarks>
     /// <returns>true if at least one target was successfully injected, otherwise false</returns>
-    private bool TryInjectTargets(Entity<BaseSolutionInjectOnEventComponent> injector, IReadOnlyList<EntityUid> targets, EntityUid? source = null)
+    private bool TryInjectTargets(Entity<BaseSolutionInjectOnEventComponent> injector, IReadOnlyList<EntityUid> targets, EntityUid? user = null)
     {
         // Make sure we have at least one target
         if (targets.Count == 0)
@@ -99,8 +100,14 @@ public sealed partial class SolutionInjectOnCollideSystem : EntitySystem
             if (!injector.Comp.PierceArmor && _inventory.TryGetSlotEntity(target, "outerClothing", out var suit) && _tag.HasTag(suit.Value, HardsuitTag))
             {
                 // Only show popup to attacker
-                if (source != null)
-                    _popup.PopupEntity(Loc.GetString(injector.Comp.BlockedByHardsuitPopupMessage, ("weapon", injector.Owner), ("target", target)), target, source.Value, PopupType.SmallCaution);
+                _popup.PopupEntity(
+                    Loc.GetString(
+                        injector.Comp.BlockedByHardsuitPopupMessage,
+                        ("weapon", injector.Owner),
+                        ("target", Identity.Entity(target, EntityManager))),
+                    target,
+                    user,
+                    PopupType.SmallCaution);
 
                 continue;
             }

Trauma-Station/Trauma-Station

Content.Server/Chemistry/EntitySystems/SolutionInjectOnEventSystem.cs
@@ -17,6 +17,7 @@ using Content.Shared.Throwing;
 using Content.Shared.Weapons.Melee.Events;
 using Robust.Shared.Collections;
 using Robust.Shared.Prototypes;
+using Content.Shared.IdentityManagement;
 
 namespace Content.Server.Chemistry.EntitySystems;
 
@@ -159,7 +160,7 @@ public sealed partial class SolutionInjectOnCollideSystem : EntitySystem
     /// </list>
     /// </remarks>
     /// <returns>true if at least one target was successfully injected, otherwise false</returns>
-    private bool TryInjectTargets(Entity<BaseSolutionInjectOnEventComponent> injector, IReadOnlyList<EntityUid> targets, EntityUid? source = null)
+    private bool TryInjectTargets(Entity<BaseSolutionInjectOnEventComponent> injector, IReadOnlyList<EntityUid> targets, EntityUid? user = null)
     {
         // Make sure we have at least one target
         if (targets.Count == 0)
@@ -203,8 +204,14 @@ public sealed partial class SolutionInjectOnCollideSystem : EntitySystem
                 if (blocked)
                 {
                     // Only show popup to attacker
-                    if (source != null)
-                        _popup.PopupEntity(Loc.GetString(injector.Comp.BlockedByArmorPopupMessage, ("weapon", injector.Owner), ("target", target)), target, source.Value, PopupType.SmallCaution);
+                    _popup.PopupEntity(
+                        Loc.GetString(
+                            injector.Comp.BlockedByArmorPopupMessage,
+                            ("weapon", injector.Owner),
+                            ("target", Identity.Entity(target, EntityManager))),
+                        target,
+                        user,
+                        PopupType.SmallCaution);
 
                     continue;
                 }