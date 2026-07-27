36bd7ff95f49d8de8b960ae5a69a5d86c69dedda
deltanedas <@deltanedas:goida.zip>
2026-07-27 04:00:15 +0100

    fix stupid untested script

--- Tools/actions_changelogs_since_last_run.py
+++ Tools/actions_changelogs_since_last_run.py
@@ -235,7 +235,7 @@ def changelog_entries_to_message_lines(entries: Iterable[ChangelogEntry]) -> lis
                 emoji = TYPES_TO_EMOJI.get(change["type"], "❓")
                 message = change["message"]
 
-                if EXPERIMENTAL_LABEL in entry["labels"]:
+                if "labels" in entry and EXPERIMENTAL_LABEL in entry["labels"]:
                     emoji = f"{emoji}{EXPERIMENTAL_EMOJI}"
 
                 message_lines.append(create_change_line(emoji, message, url))
