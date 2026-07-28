17b964c8d33cd18bfa80152fecb87c3ce37ed76d
tsarevichvanya <mrinankkiran13@gmail.com>
2026-07-28 16:33:20 -0400

    adds a system that gives the QM a fake mindshield implant if they roll headrev

---
+++ Content.Server/Revolutionary/HeadRevQMSystem.cs
@@ -0,0 +1,57 @@
+using Content.Server.Roles.Jobs;
+using Content.Shared.Implants;
+using Content.Shared.Implants.Components;
+using Content.Shared.Mind.Components;
+using Content.Shared.Mindshield.Components;
+using Content.Shared.Revolutionary.Components;
+
+namespace Content.Server.Revolutionary;
+
+/// <summary>
+/// Handles the QM's ability to get a fake mindshield implant if they roll Head Rev.
+/// </summary>
+public sealed class HeadRevQMSystem : EntitySystem
+{
+    [Dependency] private JobSystem _jobSystem = default!;
+    [Dependency] private SharedSubdermalImplantSystem _subdermal = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<HeadRevolutionaryComponent, ComponentInit>(OnHeadRevInit);
+    }
+
+    private void OnHeadRevInit(EntityUid uid, HeadRevolutionaryComponent comp, ComponentInit args)
+    {
+        if (!TryComp<MindContainerComponent>(uid, out var mindContainer) ||
+            !_jobSystem.MindTryGetJob(mindContainer.Mind, out var job) ||
+            job.ID != "Quartermaster")
+        {
+            return;
+        }
+
+        if (TryComp<FakeMindShieldComponent>(uid, out var fakeMindShield) && fakeMindShield.IsEnabled)
+            return;
+
+        if (!TryComp<ImplantedComponent>(uid, out var implanted))
+            return;
+
+        foreach (var implant in implanted.ImplantContainer.ContainedEntities)
+        {
+            if (!HasComp<MindShieldImplantComponent>(implant))
+                continue;
+
+            _subdermal.ForceRemove((uid, implanted), implant);
+            break;
+        }
+
+        _subdermal.AddImplant(uid, "FakeMindShieldImplant");
+
+        if (TryComp<FakeMindShieldComponent>(uid, out fakeMindShield))
+        {
+            fakeMindShield.IsEnabled = true;
+            Dirty(uid, fakeMindShield);
+        }
+    }
+}
