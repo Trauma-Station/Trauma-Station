1023399345823d04bcbe7b102d5a52693a9fa1b2
tsarevichvanya <mrinankkiran13@gmail.com>
2026-07-28 19:11:21 -0400

    gamerule

--- Content.Trauma.Server/Revolutionary/MindshieldRemovingAntagComponent.cs
+++ Content.Trauma.Server/Revolutionary/MindshieldRemovingAntagComponent.cs
@@ -2,7 +2,7 @@
 namespace Content.Trauma.Server.Revolutionary;
 
 /// <summary>
-/// Component that removes a real mindshield and replaces it with a fake one for antags who start with a real mindshield
+/// Gamerule component that removes a real mindshield and replaces it with a fake one for antags who start with a real mindshield
 /// </summary>
 [RegisterComponent, Access(typeof(MindshieldRemovingAntagSystem))]
 public sealed partial class MindshieldRemovingAntagComponent : Component
