3e2e16e15b675710e249c4e9d9a026ce8e1b9564
tsarevichvanya <mrinankkiran13@gmail.com>
2026-07-29 15:28:57 -0400

    Head Rev QM's fake mindshield (#3805)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ Content.Trauma.Server/Revolutionary/MindshieldRemovingAntagSystem.cs
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Server.Antag;
+using Content.Shared.Implants;
+using Content.Shared.Implants.Components;
+using Content.Shared.Mindshield.Components;
+
+namespace Content.Trauma.Server.Revolutionary;
+
+/// <summary>
+/// Handles removing a real mindshield and replacing it with a fake one for antags who start with a real mindshield
+/// </summary>
+public sealed partial class MindshieldRemovingAntagSystem : EntitySystem
+{
+    [Dependency] private SharedSubdermalImplantSystem _subdermal = default!;
+
+    [SubscribeLocalEvent]
+    private void OnAntagSelected(Entity<MindshieldRemovingAntagComponent> ent, ref AfterAntagEntitySelectedEvent args)
+    {
+        var uid = args.EntityUid;
+
+        if (HasComp<FakeMindShieldComponent>(uid))
+            return;
+
+        if (!TryComp<ImplantedComponent>(uid, out var implanted))
+            return;
+
+        foreach (var implant in implanted.ImplantContainer.ContainedEntities)
+        {
+            if (!HasComp<MindShieldImplantComponent>(implant))
+                continue;
+
+            _subdermal.ForceRemove((uid, implanted), implant);
+            break;
+        }
+
+        _subdermal.AddImplant(uid, ent.Comp.FakeMindShieldImplant);
+
+        if (TryComp<FakeMindShieldComponent>(uid, out var fakeMindShield))
+        {
+            fakeMindShield.IsEnabled = true;
+            Dirty(uid, fakeMindShield);
+        }
+    }
+}
