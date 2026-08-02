f1db0c011de23aff985631aca0ea5daf2c06eb6f
Murphy Ibbs <Murphyibbs@gmail.com>
2026-08-02 20:23:44 +0100

    unused

--- Content.Server/GameTicking/Rules/RevolutionaryRuleSystem.Trauma.cs
+++ Content.Server/GameTicking/Rules/RevolutionaryRuleSystem.Trauma.cs
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Server.RoundEnd;
-using Content.Shared.Objectives.Systems;
-using Content.Shared.Revolutionary.Components;
-using Content.Trauma.Common.CCVar;
 using Content.Trauma.Common.GameTicking;
-using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server.GameTicking.Rules;
