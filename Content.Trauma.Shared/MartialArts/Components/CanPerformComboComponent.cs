2132b5db4a27e09b6a402c829b5126e09162b317
M <mickers7@gmail.com>
2026-08-13 00:49:42 -0700

    fixed comments

--- Content.Trauma.Shared/MartialArts/Components/CanPerformComboComponent.cs
+++ Content.Trauma.Shared/MartialArts/Components/CanPerformComboComponent.cs
@@ -10,24 +10,51 @@ namespace Content.Trauma.Shared.MartialArts.Components;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class CanPerformComboComponent : Component
 {
+    /// <summary>
+    /// Gets hit target.
+    /// </summary>
     [DataField]
     public EntityUid? CurrentTarget;
 
+    /// <summary>
+    /// Combo memory size.
+    /// </summary>
     [DataField]
     public int LastAttacksLimit = 4;
 
+    /// <summary>
+    /// Move storage. Stores the last attacks performed, used for combo performing and checking.
+    /// </summary>
     [DataField, AutoNetworkedField]
     public List<ComboAttackType> LastAttacks = new();
 
+    /// <summary>
+    /// Move storage.
+    /// </summary>
     [DataField]
     public List<ComboAttackType>? LastAttacksSaved = new();
 
+    /// <summary>
+    /// Current combo list.
+    /// </summary>
     [ViewVariables]
     public List<ComboPrototype> AllowedCombos = new();
 
+    /// <summary>
+    /// Combo storage from yaml.
+    /// </summary>
     [DataField]
     public List<ProtoId<ComboPrototype>> RoundstartCombos = new();
 
+    /// <summary>
+    /// Time since last hit.
+    /// </summary>
     [DataField]
     public TimeSpan ResetTime = TimeSpan.Zero;
+
+    /// <summary>
+    /// Momentum counter.
+    /// </summary>
+    [DataField]
+    public int Momentum;
 }
