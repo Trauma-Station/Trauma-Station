9bc145d026df675bfadded621d70ade564252cd0
Aviu00 <aviu00@protonmail.com>
2026-08-13 14:04:39 +0300

    Fix.

--- Content.Trauma.Shared/Heretic/Systems/Side/SharedVoidCloakSystem.cs
+++ Content.Trauma.Shared/Heretic/Systems/Side/SharedVoidCloakSystem.cs
@@ -20,10 +20,10 @@ public abstract partial class SharedVoidCloakSystem : EntitySystem
     [SubscribeLocalEvent]
     private void OnBeforeHeatExchange(Entity<VoidCloakComponent> ent, ref InventoryRelayedEvent<BeforeHeatExchangeEvent> args)
     {
-        if (ent.Comp.Transparent || args.Args.OurTemp > args.Args.OtherTemp)
+        if (ent.Comp.Transparent)
             return;
 
-        args.Args.Cancelled = true;
+        args.Args.HeatTransferModifier *= 0.001f;
     }
 
     [SubscribeLocalEvent]
