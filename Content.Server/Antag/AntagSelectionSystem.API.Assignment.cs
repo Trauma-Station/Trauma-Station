4455f9b14b4f315bc65c41845b6f43431d7a487f
deltanedas <@deltanedas:goida.zip>
2026-08-13 13:18:49 +0100

    fix midround shadowling not spawning anything

--- Content.Server/Antag/AntagSelectionSystem.API.Assignment.cs
+++ Content.Server/Antag/AntagSelectionSystem.API.Assignment.cs
@@ -396,7 +396,7 @@ public sealed partial class AntagSelectionSystem
     {
         if (proto.SpawnerPrototype is not { } spawnerPrototype)
         {
-            Debug.Assert(!assert, $"Tried to spawn a ghost role for {proto.ID}, but it had no prototype!");
+            Debug.Assert($"Tried to spawn a ghost role for {proto.ID}, but it had no prototype!"); // Trauma - always assert regardless of param, this is a programmer error
             return;
         }
 
