1892abdac319ec48d14baeb3f8a478b305cb00e4
M <mickers7@gmail.com>
2026-08-18 12:09:34 -0700

    addressed change requests

--- Content.Trauma.Shared/MartialArts/MartialArtsSystem.Combos.cs
+++ Content.Trauma.Shared/MartialArts/MartialArtsSystem.Combos.cs
@@ -5,6 +5,7 @@ using Content.Shared.EntityConditions;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
+using Content.Shared.Popups;
 using Content.Trauma.Common.MartialArts;
 using Content.Trauma.Shared.MartialArts.Components;
 
@@ -17,6 +18,7 @@ public partial class MartialArtsSystem
 {
     [Dependency] private MobStateSystem _mobState = default!;
     [Dependency] private SharedEntityConditionsSystem _conditions = default!;
+    [Dependency] private SharedPopupSystem _popup = default!;
 
     [SubscribeLocalEvent]
     private void OnInit(Entity<CanPerformComboComponent> ent, ref ComponentInit args)
@@ -65,8 +67,11 @@ public partial class MartialArtsSystem
             var proto = ProtoMan.Index(queued);
             var level = _knowledge.GetLevel(ent.Owner);
 
-            if (!CheckCombo(ent, proto, level, user, args.Target))
+            if (!CheckCombo(ent, proto, level, user, args.Target, out var failMessage))
+            {
+                PopupFail(failMessage, user);
                 return;
+            }
 
             PerformCombo(user, args.Target, proto, ent, level);
             comboActions.QueuedPrototype = null;
@@ -82,22 +87,37 @@ public partial class MartialArtsSystem
         var performer = args.Performer;
         var level = _knowledge.GetLevel(ent.Owner);
 
+        LocId? failMessage = null;
         foreach (var proto in ent.Comp.AllowedCombos)
         {
-            if (!CheckCombo(ent, proto, level, performer, target))
+            if (!CheckCombo(ent, proto, level, performer, target, out var fail))
+            {
+                failMessage ??= fail;
                 continue;
+            }
 
             PerformCombo(performer, target, proto, ent, level);
-            break; // found the combo
+            return; // found the combo
         }
+
+        PopupFail(failMessage, performer);
+    }
+
+    private void PopupFail(LocId? message, EntityUid user)
+    {
+        if (message is { } id)
+            _popup.PopupCursor(Loc.GetString(id), user, PopupType.MediumCaution);
     }
 
     private bool CheckCombo(Entity<CanPerformComboComponent> ent,
         ComboPrototype proto,
         int level,
         EntityUid user,
-        EntityUid target)
+        EntityUid target,
+        out LocId? failMessage)
     {
+        failMessage = null;
+
         var sum = ent.Comp.LastAttacks.Count - proto.AttackTypes.Count;
         if (proto.AttackTypes.Count <= 0 || sum < 0)
             return false;
@@ -106,10 +126,21 @@ public partial class MartialArtsSystem
         var attackList = proto.AttackTypes.AsEnumerable();
 
         if (level < proto.LevelRequired || (level > proto.LevelExceeded && proto.LevelExceeded > 0) ||
-            !list.SequenceEqual(attackList) ||
-            !_conditions.TryConditions(user, proto.UserConditions, sourceEnt: user) ||
-            !_conditions.TryConditions(target, proto.Conditions, sourceEnt: user))
+            !list.SequenceEqual(attackList))
             return false;
+
+        // only complain once the attacks lined up, or every combo you don't know would yell at you
+        if (!_conditions.TryConditions(user, proto.UserConditions, out var failedUser, sourceEnt: user))
+        {
+            failMessage = failedUser?.FailMessage;
+            return false;
+        }
+
+        if (!_conditions.TryConditions(target, proto.Conditions, out var failedTarget, sourceEnt: user))
+        {
+            failMessage = failedTarget?.FailMessage;
+            return false;
+        }
 
         return true;
     }
