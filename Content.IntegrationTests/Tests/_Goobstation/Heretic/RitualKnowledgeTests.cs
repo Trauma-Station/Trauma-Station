edf6b4ec1efb495d8c92e170d8fc5f896868f235
deltanedas <@deltanedas:goida.zip>
2026-08-18 05:42:09 +0100

    update tests

--- Content.IntegrationTests/Tests/_Goobstation/Heretic/RitualKnowledgeTests.cs
+++ Content.IntegrationTests/Tests/_Goobstation/Heretic/RitualKnowledgeTests.cs
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.IntegrationTests.Fixtures;
-using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Shared.Whitelist;
 using Content.Trauma.Shared.Heretic.Prototypes;
-using Robust.Shared.Prototypes;
 using System.Linq;
 
 namespace Content.IntegrationTests.Tests._Goobstation.Heretic;
 
-[TestFixture, TestOf(typeof(Trauma.Shared.Heretic.Components.Side.HereticKnowledgeRitualComponent))]
+[TestFixture, TestOf(typeof(RitualIngredientDatasetPrototype)]
 public sealed partial class RitualKnowledgeTests : GameTest
 {
     [SidedDependency(Side.Server)] private EntityWhitelistSystem _whitelist = default!;
@@ -17,20 +14,20 @@ public sealed partial class RitualKnowledgeTests : GameTest
     [Test]
     public async Task ValidateTagsHaveItems()
     {
-        await Server.WaitAssertion(() =>
+        var ingredients = SProtoMan.EnumeratePrototypes<RitualIngredientDatasetPrototype>()
+            .SelectMany(x => x.Ingredients)
+            .ToHashSet();
+
+        // TODO: this can still contain completely unobtainable entities
+        foreach (var proto in SProtoMan.EnumeratePrototypes<EntityPrototype>())
         {
-            var ingredients = SProtoMan.EnumeratePrototypes<RitualIngredientDatasetPrototype>()
-                .SelectMany(x => x.Ingredients)
-                .ToHashSet();
+            ingredients.RemoveWhere(x => _whitelist.CheckBoth(proto, x.Blacklist, x.Whitelist));
+            if (ingredients.Count == 0)
+                return;
+        }
 
-            foreach (var proto in SProtoMan.EnumeratePrototypes<EntityPrototype>())
-            {
-                ingredients.RemoveWhere(x => _whitelist.CheckBoth(proto, x.Blacklist, x.Whitelist));
-            }
-
-            Assert.That(ingredients,
-                Is.Empty,
-                $"The following ritual ingredient tags are not provided by any available entities: {string.Join(", ", ingredients.Select(x => x.Name))}");
-        });
+        Assert.That(ingredients,
+            Is.Empty,
+            $"The following ritual ingredient tags are not provided by any available entities: {string.Join(", ", ingredients.Select(x => x.Name))}");
     }
 }
