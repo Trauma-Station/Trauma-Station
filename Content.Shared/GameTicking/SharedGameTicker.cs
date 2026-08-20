356e41ca6ad899cc08e40fa016598de1164423d8
Nothing98512 <123211206+Nothing98512@users.noreply.github.com>
2026-08-19 22:02:59 -0400

    also forgot to set overflow to actually be D-Class

--- Content.Shared/GameTicking/SharedGameTicker.cs
+++ Content.Shared/GameTicking/SharedGameTicker.cs
@@ -30,7 +30,7 @@ namespace Content.Shared.GameTicking
         // See ideally these would be pulled from the job definition or something.
         // But this is easier, and at least it isn't hardcoded.
         //TODO: Move these, they really belong in StationJobsSystem or a cvar.
-        public static readonly ProtoId<JobPrototype> FallbackOverflowJob = "Passenger";
+        public static readonly ProtoId<JobPrototype> FallbackOverflowJob = "DClass";
 
         //Changed overflow role from passenger to d-class
         public const string FallbackOverflowJobName = "job-name-dclass";
