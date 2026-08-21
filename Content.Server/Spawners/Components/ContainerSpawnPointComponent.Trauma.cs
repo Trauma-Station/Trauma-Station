5dbd0ae733984514b8442d55ba4172ab99c0411a
Nothing98512 <123211206+Nothing98512@users.noreply.github.com>
2026-08-20 21:26:18 -0400

    Fuck me why is this like this

---
+++ Content.Server/Spawners/Components/ContainerSpawnPointComponent.Trauma.cs
@@ -0,0 +1,14 @@
+﻿using Content.Server.Spawners.EntitySystems;
+using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.Spawners.Components;
+
+public sealed partial class ContainerSpawnPointComponent : Component, ISpawnPoint
+{
+    /// <summary>
+    /// Extra Jobs that can spawn at a spawnpoint
+    /// </summary>
+    [DataField]
+    public List<ProtoId<JobPrototype>> ExtraJobs = new();
+}
