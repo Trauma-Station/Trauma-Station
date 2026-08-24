5ab5c5bf1c73b2822fa3da29843466aaaa92bb95
John <50085876+AreYouConfused@users.noreply.github.com>
2026-08-24 04:19:58 -0400

    adjust fire energy and humaniod acceptable heat values (#45205)
    * adjust fire's energy output
    
    * adjust mob heat damage thresholds, fix moth guidebook
    
    * named the number

--- Content.Server/Atmos/EntitySystems/FlammableSystem.cs
+++ Content.Server/Atmos/EntitySystems/FlammableSystem.cs
@@ -63,9 +63,10 @@ namespace Content.Server.Atmos.EntitySystems
 
         private readonly Dictionary<Entity<FlammableComponent>, float> _fireEvents = new();
 
+        private const int FirestackEnergy = 37500; // joules release when on fire
+
         public override void Initialize()
         {
-            InitTrauma(); // Trauma
             UpdatesAfter.Add(typeof(AtmosphereSystem));
 
             SubscribeLocalEvent<FlammableComponent, MapInitEvent>(OnMapInit);
@@ -531,10 +532,10 @@ namespace Content.Server.Atmos.EntitySystems
                     var source = EnsureComp<IgnitionSourceComponent>(uid);
                     _ignitionSourceSystem.SetIgnited((uid, source));
 
-                    // <Trauma> - check fire immune and use cvar instead of hardcoded energy
-                    var isImmune = _fireImmuneQuery.HasComp(uid); // Trauma
+                    // <Trauma> - check fire immune first
+                    var isImmune = _fireImmuneQuery.HasComp(uid);
                     if (!isImmune)
-                        _temperatureSystem.ChangeHeat(uid, _addHeatFirestack * flammable.FireStacks, false);
+                        _temperatureSystem.ChangeHeat(uid, FirestackEnergy * flammable.FireStacks, false);
                     // </Trauma>
 
                     var ev = new GetFireProtectionEvent(uid); // Trauma - added uid
