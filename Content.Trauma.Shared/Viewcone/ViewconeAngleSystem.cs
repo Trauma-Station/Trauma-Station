a62e0ddc8ee7828764752a3bd128427289c68f5a
Evoluxionary <Isaac_jarrett@icloud.com>
2026-08-25 11:55:16 -0400

    added helmet prototype

--- Content.Trauma.Shared/Viewcone/ViewconeAngleSystem.cs
+++ Content.Trauma.Shared/Viewcone/ViewconeAngleSystem.cs
@@ -94,7 +94,7 @@ public sealed partial class ViewconeAngleSystem : EntitySystem
 [ByRefEvent]
 public record struct ModifyViewconeAngleEvent() : IInventoryRelayEvent
 {
-    public SlotFlags TargetSlots => SlotFlags.HEAD | SlotFlags.EYES | SlotFlags.MASK | SlotFlags.OUTERCLOTHING;
+    public SlotFlags TargetSlots => SlotFlags.HEAD | SlotFlags.EYES | SlotFlags.MASK;
 
     private float _angleModifier = 1f;
 
