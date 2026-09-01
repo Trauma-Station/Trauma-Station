9cedb0c9bcc83e1c85c42946c3226ef0d250a557
deltanedas <@deltanedas:goida.zip>
2026-09-01 19:33:22 +0100

    !

--- Content.Trauma.Shared/Knowledge/Systems/SharedKnowledgeSystem.cs
+++ Content.Trauma.Shared/Knowledge/Systems/SharedKnowledgeSystem.cs
@@ -469,9 +469,10 @@ public abstract partial class SharedKnowledgeSystem : CommonKnowledgeSystem
     {
         if (GetContainer(target) is not { } ent ||
             ent.Comp.Holder is not { } holder ||
+            ent.Comp.Container is not { } container ||
             GetKnowledge(ent, id) is not { } unit ||
             unit.Comp.Unremoveable && !force ||
-            !_container.Remove(unit.Owner, ent.Comp.Container, force: force))
+            !_container.Remove(unit.Owner, container, force: force))
             return null;
 
         ent.Comp.KnowledgeDict.Remove(id);
