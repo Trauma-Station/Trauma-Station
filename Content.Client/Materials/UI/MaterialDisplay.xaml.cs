0c6000a9f93f61be47c778e1859d04f0d9a63cc2
bru(s)h <202476090+GlupiyBrush@users.noreply.github.com>
2026-09-02 05:38:27 +0000

    Fixes localization warning in MaterialDisplay (#45637)
    Fix

--- Content.Client/Materials/UI/MaterialDisplay.xaml.cs
+++ Content.Client/Materials/UI/MaterialDisplay.xaml.cs
@@ -87,7 +87,7 @@ public sealed partial class MaterialDisplay : PanelContainer
             {
                 Name = $"{sheetsToEject}",
                 Access = AccessLevel.Public,
-                Text = $"{sheetsToEject}", // Trauma - remove loc for a fucking number, an iq too high
+                Text = sheetsToEject.ToString(),
                 MinWidth = 45,
                 StyleClasses = { styleClass }
             };
