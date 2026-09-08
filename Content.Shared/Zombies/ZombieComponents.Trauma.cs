100e5e82cd8e53354445d48ea48fd5bd96ad4314
notemare <luminardm@gmail.com>
2026-09-08 10:41:02 -0500

    Moved ForcedLanguage datafield in ZombieComponent.cs to new ZombieComponents.Trauma.cs

---
+++ Content.Shared/Zombies/ZombieComponents.Trauma.cs
@@ -0,0 +1,19 @@
+﻿// <Trauma>
+using Content.Trauma.Common.Language;
+// </Trauma>
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Zombies;
+public sealed partial class ZombieComponent : Component
+{
+    /// <summary>
+    ///     This is the forced language a zombie should have when they are zombified and try to speak.
+    /// </summary>
+    /// <remarks>
+    ///     This is intended as a fallback to prevent zombies from using sign language or any other
+    ///     language that bypasses accent filter, and it prevents them from understanding everything
+    ///     else while being zombified.
+    /// </remarks>
+    [DataField]
+    public ProtoId<LanguagePrototype> ForcedLanguage = "Zombish";
+}
