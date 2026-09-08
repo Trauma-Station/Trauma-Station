22a340b8ff6c3cac13986832978177f376c5454d
notemare <luminardm@gmail.com>
2026-09-08 01:43:24 -0500

    Revert "Put datafield for forcing zombie language into new ZombieComponent.Trauma.cs"
    This reverts commit 833c2f814c874355b048475682f64d09f57873fe.

--- Content.Shared/Zombies/ZombieComponent.cs
+++ Content.Shared/Zombies/ZombieComponent.cs
@@ -20,6 +20,17 @@ namespace Content.Shared.Zombies;
 public sealed partial class ZombieComponent : Component
 {
     /// <summary>
+    ///     This is the forced language a zombie should have when they are zombified and try to speak.
+    /// </summary>
+    /// <remarks>
+    ///     This is intended as a fallback to prevent zombies from using sign language or any other
+    ///     language that bypasses accent filter, and it prevents them from understanding everything
+    ///     else while being zombified.
+    /// </remarks>
+    [DataField]
+    public ProtoId<LanguagePrototype> ForcedLanguage = "Zombish";
+
+    /// <summary>
     /// The baseline infection chance you have if you have no protective gear
     /// </summary>
     [DataField]
