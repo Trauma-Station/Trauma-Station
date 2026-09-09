91b4d599bb13be7bfd4aec98c888200b532051f8
yglop <95057024+yglop@users.noreply.github.com>
2026-09-09 17:22:52 +0500

    it predicts now and also shows in techfab after research can we call it a day already?

--- Content.Trauma.Shared/ReactiveArmour/ReactiveArmourComponent.cs
+++ Content.Trauma.Shared/ReactiveArmour/ReactiveArmourComponent.cs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Content.Shared.EntityEffects;
 
 namespace Content.Trauma.Shared.ReactiveArmour;
@@ -7,7 +8,7 @@ namespace Content.Trauma.Shared.ReactiveArmour;
 /// <summary>
 /// Checks if enought time have passed to activate reactive armour behavior
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, AutoGenerateComponentPause]
 public sealed partial class ReactiveArmourComponent : Component
 {
     [DataField(required: true)]
@@ -16,6 +17,6 @@ public sealed partial class ReactiveArmourComponent : Component
     [DataField(required: true)]
     public TimeSpan ActivationDelay;
 
-    [DataField, TimeOffsetSerializer, AutoPausedField, AutoNetworkedField]
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField, AutoNetworkedField]
     public TimeSpan LastActivated = default;
 }
