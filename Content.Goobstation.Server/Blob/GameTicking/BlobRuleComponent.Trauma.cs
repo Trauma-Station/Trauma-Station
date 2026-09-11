270ddb42d0313519cbd927845cef3eae448d8000
notemare <luminardm@gmail.com>
2026-09-11 18:25:21 -0500

    Removed unused code

--- Content.Goobstation.Server/Blob/GameTicking/BlobRuleComponent.Trauma.cs
+++ Content.Goobstation.Server/Blob/GameTicking/BlobRuleComponent.Trauma.cs
@@ -1,8 +1,5 @@
 ﻿// SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Goobstation.Shared.Blob;
-using Content.Server.GameTicking.Rules;
-using Content.Shared.Mind;
 using Robust.Shared.Audio;
 
 namespace Content.Goobstation.Server.Blob.GameTicking;
