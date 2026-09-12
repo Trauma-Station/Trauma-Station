ddb28d0ee6b5efb15704875d27c1d3034d4e810a
Aviu00 <aviu00@protonmail.com>
2026-09-12 07:52:32 +0300

    face holding back tears

---
+++ Content.Trauma.Server/Spawners/Components/ConditionalSpawnRuleComponent.cs
@@ -0,0 +1,12 @@
+namespace Content.Trauma.Server.Spawners.Components;
+
+/// <summary>
+/// Component added to gamerules that load maps. Used to determine which entities to spawn on
+/// <see cref="GameRuleSpawnerComponent"/> spawners based on Key
+/// </summary>
+[RegisterComponent]
+public sealed partial class ConditionalSpawnRuleComponent : Component
+{
+    [DataField(required: true)]
+    public string Key;
+}
