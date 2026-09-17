e3e90c5111a29a534ff96c3e302b46916976f068
yglop <95057024+yglop@users.noreply.github.com>
2026-09-17 16:38:03 +0500

    i can just do that

--- Content.Trauma.Shared/ReduseStackSizeOnUse/ReduseStackSizeOnUseComponent.cs
+++ Content.Trauma.Shared/ReduseStackSizeOnUse/ReduseStackSizeOnUseComponent.cs
@@ -6,5 +6,4 @@ namespace Content.Trauma.Shared.ReduseStackSizeOnUse;
 /// Reduses stack size by 1 or delets the stack on use in hand.
 /// </summary>
 [RegisterComponent]
-public sealed partial class ReduseStackSizeOnUseComponent : Component
-{}
+public sealed partial class ReduseStackSizeOnUseComponent : Component;
