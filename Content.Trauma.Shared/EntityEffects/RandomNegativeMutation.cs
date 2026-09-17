4337684b415b137257703c67386929b81269d3b5
deltanedas <39013340+deltanedas@users.noreply.github.com>
2026-09-17 15:11:12 +0100

    nerf by buff rad storms (#4641)
    Co-authored-by: deltanedas <@deltanedas:goida.zip>

--- Content.Trauma.Shared/EntityEffects/RandomNegativeMutation.cs
+++ Content.Trauma.Shared/EntityEffects/RandomNegativeMutation.cs
@@ -12,7 +12,14 @@ namespace Content.Trauma.Shared.EntityEffects;
 /// Adds a random negative mutation to the target entity.
 /// Does nothing for mutations which are already present or conflict with existing ones.
 /// </summary>
-public sealed partial class RandomNegativeMutation : EntityEffectBase<RandomNegativeMutation>;
+public sealed partial class RandomNegativeMutation : EntityEffectBase<RandomNegativeMutation>
+{
+    /// <summary>
+    /// Multiplier on difficulty for sequencing the mutation.
+    /// </summary>
+    [DataField]
+    public float Difficulty = 1f;
+}
 
 public sealed partial class RandomNegativeMutationEffectSystem : EntityEffectSystem<MutatableComponent, RandomNegativeMutation>
 {
@@ -24,6 +31,6 @@ public sealed partial class RandomNegativeMutationEffectSystem : EntityEffectSys
         var rand = SharedRandomExtensions.PredictedRandom(_timing, GetNetEntity(ent));
         var mutation = rand.Pick(_mutation.NegativeMutations);
         _mutation.AddMutation(ent.AsNullable(), mutation, user: args.User,
-            automatic: false, predicted: args.Predicted);
+            automatic: false, predicted: args.Predicted, difficulty: args.Effect.Difficulty);
     }
 }
