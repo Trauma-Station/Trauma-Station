8ff65f5bc7f2d9d25636aa931dc08a9e15e16d90
No Elka <no.elka.the.god@gmail.com>
2026-09-19 14:50:57 +0300

    Nullcheck

--- Content.Trauma.Server/GameTicking/Rules/XenomorphsRuleSystem.cs
+++ Content.Trauma.Server/GameTicking/Rules/XenomorphsRuleSystem.cs
@@ -244,7 +244,7 @@ public sealed partial class XenomorphsRuleSystem : GameRuleSystem<XenomorphsRule
         if (!component.Announced && component.AnnouncementTime <= _timing.CurTime)
         {
             component.Announced = true;
-            var queenUid = GetXenomorphs(component, "Queen").First; // There should only be one queen so this works.
+            var queenUid = GetXenomorphs(component, "Queen").First(); // There should only be one queen so this works.
 
             if (!string.IsNullOrEmpty(component.Announcement))
                 _chat.DispatchGlobalAnnouncement(Loc.GetString(component.Announcement), component.Sender != null ? Loc.GetString(component.Sender) : null, colorOverride: component.AnnouncementColor);
@@ -298,6 +298,8 @@ public sealed partial class XenomorphsRuleSystem : GameRuleSystem<XenomorphsRule
             || xenomorphs.Count / (float) (xenomorphs.Count + humans.Count) < component.XenomorphsShuttleCallPercentage)
             return;
 
+        if (GetXenomorphs(component, "Queen").FirstOrNull() is not { } queenUid) return; // No queen no win
+
         _roundEnd.DoRoundEndBehavior(
             RoundEndBehavior.ShuttleCall,
             component.ShuttleCallTime,
@@ -305,7 +307,6 @@ public sealed partial class XenomorphsRuleSystem : GameRuleSystem<XenomorphsRule
             component.RoundEndTextShuttleCall,
             component.RoundEndTextAnnouncement
         );
-        var queenUid = GetXenomorphs(component, "Queen").First;
         _sound.StopStationEventMusic(queenUid, StationEventMusicType.Xenomorph);
         _sound.DispatchStationEventMusic(queenUid, component.XenomorphTakeoverSound, StationEventMusicType.Xenomorph, component.XenomorphTakeoverSound.Params);
 
