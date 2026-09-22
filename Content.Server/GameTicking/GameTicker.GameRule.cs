62c0e1cee5e83934155d9fda18827db61f04fd1f
Nothing98512 <123211206+Nothing98512@users.noreply.github.com>
2026-09-22 19:29:38 -0400

    dogshit

--- Content.Server/GameTicking/GameTicker.GameRule.cs
+++ Content.Server/GameTicking/GameTicker.GameRule.cs
@@ -439,13 +439,13 @@ public sealed partial class GameTicker
             var minPlayers = gameRule.MinPlayers;
             var name = ToPrettyString(uid);
 
-            if (ReadyPlayerCount() >= minPlayers)
+            if (ReadyPlayerCount() >= minPlayers) // ReadyPlayerCount() used to be args.Players.Length
                 continue;
 
             if (gameRule.CancelPresetOnTooFewPlayers)
             {
                 _chatManager.SendAdminAnnouncement(Loc.GetString("preset-not-enough-ready-players",
-                    ("readyPlayersCount", ReadyPlayerCount()),
+                    ("readyPlayersCount", ReadyPlayerCount()), // ReadyPlayerCount() used to be args.Players.Length
                     ("minimumPlayers", minPlayers),
                     ("presetName", name)));
                 args.Cancel();
