43f9465d7acd42460cc39e3cec3013febbece9c1
deltanedas <@deltanedas:goida.zip>
2026-09-23 20:01:09 +0100

    !

--- Content.Trauma.Client/BloodCult/ClientBloodCultSystem.cs
+++ Content.Trauma.Client/BloodCult/ClientBloodCultSystem.cs
@@ -26,7 +26,7 @@ public sealed partial class ClientBloodCultSystem : BloodCultSystem
     [SubscribeLocalEvent]
     private void OnPentagramAdded(EntityUid uid, PentagramComponent component, ComponentStartup args)
     {
-        if (!TryComp<SpriteComponent>(uid, out var sprite) || sprite.LayerMapTryGet(PentagramKey.Key, out _))
+        if (!TryComp<SpriteComponent>(uid, out var sprite) || _sprite.LayerMapTryGet((uid, sprite), PentagramKey.Key, out _))
             return;
 
         var bounds = _sprite.GetLocalBounds((uid, sprite));
