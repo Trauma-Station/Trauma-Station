3351be0a41d72facd793c4dc508044a376409594
Nothing98512 <123211206+Nothing98512@users.noreply.github.com>
2026-09-24 19:21:55 -0400

    test

--- Content.Server/GameTicking/Rules/SecretRuleSystem.cs
+++ Content.Server/GameTicking/Rules/SecretRuleSystem.cs
@@ -83,11 +83,17 @@ public sealed partial class SecretRuleSystem : GameRuleSystem<SecretRuleComponen
         {
             var accumulated = 0f;
             var rand = _random.NextFloat(sum);
+            string minReadyKeyCheck = "";
             foreach (var (key, weight) in options)
             {
+                // <Trauma>
                 accumulated += weight;
                 if (accumulated < rand)
+                {
+                    minReadyKeyCheck = key;
                     continue;
+                }
+                // </Trauma>
 
                 if (!ProtoMan.TryIndex(key, out selectedPreset))
                     Log.Error($"Invalid preset {selectedPreset} in secret rule weights: {weights}");
@@ -96,6 +102,14 @@ public sealed partial class SecretRuleSystem : GameRuleSystem<SecretRuleComponen
                 sum -= weight;
                 break;
             }
+            // <Trauma> Removes any presets that which the minimum players is not the ready count.
+            if (minReadyKeyCheck != null && selectedPreset != null &&
+                selectedPreset.MinPlayers < GameTicker.ReadyPlayerCountEffective())
+            {
+                options.Remove(minReadyKeyCheck);
+                continue;
+            }
+            // </Trauma>
 
             if (CanPick(selectedPreset, players))
             {
