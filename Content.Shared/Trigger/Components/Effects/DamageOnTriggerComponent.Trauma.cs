468b4c6c3db1349a0dc640175f54e89283562897
TechnicFighter <tomgodmartins@gmail.com>
2026-09-25 10:35:41 +0100

    there

--- Content.Shared/Trigger/Components/Effects/DamageOnTriggerComponent.Trauma.cs
+++ Content.Shared/Trigger/Components/Effects/DamageOnTriggerComponent.Trauma.cs
@@ -19,5 +19,4 @@ public sealed partial class DamageOnTriggerComponent
     /// </summary>
     [DataField]
     public TargetBodyPart? TargetPart;
-
 }
--- Content.Shared/Trigger/Systems/DamageOnTriggerSystem.cs
+++ Content.Shared/Trigger/Systems/DamageOnTriggerSystem.cs
@@ -1,6 +1,5 @@
 // <Trauma>
 using Content.Shared.Whitelist;
-using Robust.Shared.Containers;
 // </Trauma>
 using Content.Shared.Damage;
 using Content.Shared.Trigger.Components.Effects;
@@ -11,25 +10,12 @@ public sealed partial class DamageOnTriggerSystem : XOnTriggerSystem<DamageOnTri
 {
     // <Trauma>
     [Dependency] private EntityWhitelistSystem _whitelist = default!;
-    [Dependency] private SharedContainerSystem _containers = default!;
     // </Trauma>
     [Dependency] private Damage.Systems.DamageableSystem _damageableSystem = default!;
 
     protected override void OnTrigger(Entity<DamageOnTriggerComponent> ent, EntityUid target, ref TriggerEvent args)
     {
         // <Trauma>
-        if (ent.Comp.TargetUser)
-        {
-            if (args.User is { } user)
-                target = user;
-
-            else if (_containers.TryGetContainingContainer(ent.Owner, out var container))
-                target = container.Owner;
-
-            else
-                return;
-        }
-
         if (!_whitelist.CheckBoth(target, ent.Comp.Blacklist, ent.Comp.Whitelist))
             return;
         // </Trauma>
@@ -38,9 +24,7 @@ public sealed partial class DamageOnTriggerSystem : XOnTriggerSystem<DamageOnTri
         var ev = new BeforeDamageOnTriggerEvent(damage, target);
         RaiseLocalEvent(ent.Owner, ref ev);
 
-        var canMiss = ent.Comp.TargetPart == null; // Trauma
-
-        args.Handled |= _damageableSystem.TryChangeDamage(target, ev.Damage, ent.Comp.IgnoreResistances, origin: ent.Owner, targetPart: ent.Comp.TargetPart, canMiss: canMiss); // Trauma - added targetPart and canMiss
+        args.Handled |= _damageableSystem.TryChangeDamage(target, ev.Damage, ent.Comp.IgnoreResistances, origin: ent.Owner, targetPart: ent.Comp.TargetPart, canMiss: ent.Comp.TargetPart == null); // Trauma - added targetPart and canMiss
     }
 }
 
